07da11cf131843e4b55bf797f71f4cc71f37717a
fguo <fguo@columbia.edu>
2005-08-25 17:01:22 +0000

    Export functionality.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@177 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/SpearmanRankDistance.java
+++ src/org/geworkbench/util/SpearmanRankDistance.java
@@ -81,7 +81,7 @@ public class SpearmanRankDistance implements Distance {
      * @param data double[]
      * @return double[]
      */
-    private static double[] getRank(double[] data) {
+    public static double[] getRank(double[] data) {
         int n = data.length;
         double[] rank = new double[n];
         Integer[] index = new Integer[n]; // to use the customized sort has to be class
@@ -105,29 +105,36 @@ public class SpearmanRankDistance implements Distance {
         return rank;
     }
 
-    private double crank(double w[]) {
-        int j = 0, ji, jt;
-        double t, rank;
-        double s = 0.0;
-        int n = w.length;
-        while (j < n - 1) {
-            if (w[j + 1] != w[j]) { // Not a tie.
-                w[j] = j;
-                ++j;
-            } else {
-                for (jt = j + 1; jt < n && w[jt] == w[j]; jt++)
-                    ;
-                rank = 0.5 * (j + jt - 1);
-                for (ji = j; ji <= (jt - 1); ji++)
-                    w[ji] = rank;
-                t = jt - j;
-                s += t * t * t - t;
-                j = jt;
-            }
-        }
+    /**
+     * getRank computes the ranks for the array <code>a</code>; for tied
+     * elements, their average rank is used for them.
+     *
+     * @param data double[]
+     * @param rank double[] - allocated by caller, will store the ranks
+     * @return double - sum of m^3 - m for correction
+     */
+    public static double getRank(double[] data, double[] rank) {
+        int n = data.length;
+        Integer[] index = new Integer[n]; // to use the customized sort has to be class
+        for (int i = 0; i < n; i++) index[i] = i;
+        Arrays.sort(index, new IndexSortComparator(data));
 
-        if (j == n - 1)
-            w[n - 1] = n - 1;
+        /* Build a rank table */
+        for (int i = 0; i < n; i++) rank[index[i]] = i;
+        /* Fix for equal ranks */
+        int i = 0;
+        double s = 0.0;
+        while (i < n) {
+            int m;
+            double value = data[index[i]];
+            int j = i + 1;
+            while (j < n && data[index[j]] == value) j++;
+            m = j - i; /* number of equal ranks found */
+            value = rank[index[i]] + (m - 1) / 2.;
+            for (j = i; j < i + m; j++) rank[index[j]] = value;
+            i += m;
+            s += m * m * m - m;
+        }
         return s;
     }
 
