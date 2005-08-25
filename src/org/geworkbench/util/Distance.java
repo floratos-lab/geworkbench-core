020d6066bbbdcdff4f704ad9f7c40294d9ed1fd8
fguo <fguo@columbia.edu>
2005-08-25 16:56:43 +0000

    Update comment.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@176 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/Distance.java
+++ src/org/geworkbench/util/Distance.java
@@ -11,26 +11,11 @@ package org.geworkbench.util;
  * <p/>
  * <p>Company: Columbia University</p>
  * <p/>
- * <p><b>Purpose</b>: Compute the distances between two vectors given by iterators.
+ * <p><b>Purpose</b>: Compute the distances between two vectors given by iterators or two arrays.
  * (e.g., the vectors could be genes or microarrays.)
  * The distance could be Euclidean, Pearson, or Spearman. <emph>If one vector is longer
  * than the other, we compute upto the shorter one and the rest values of the longer
  * one are ignored. If a value is not a number, we silently skip the value.</emph> </p>
- * <p/>
- * <p><b>Note for microarray set</b>. (1) To computer distance between two
- * microarrays:
- * <pre>
- *       Iterator a1 = mset.valueIteratorForMicroarray(i);
- *       Iterator a2 = mset.valueIteratorForMicroarray(j);
- *       double d = Distance.euclidean(a1, a2);
- * </pre>
- * (2) To computer distance between two genes:
- * <pre>
- *       Iterator a1 = mset.valueIteratorForGene(i);
- *       Iterator a2 = mset.valueIteratorForGene(j);
- *       double d = Distance.euclidean(a1, a2);
- * </pre>
- * </p>
  *
  * @author Frank Wei Guo
  * @version 3.0
