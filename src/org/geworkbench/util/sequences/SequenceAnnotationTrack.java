86fec0618dcf374cac42b67d32add90ea5fef10c
pavel <pavel@columbia.edu>
2005-09-09 14:02:05 +0000

    no message
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@261 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/sequences/SequenceAnnotationTrack.java
+++ src/org/geworkbench/util/sequences/SequenceAnnotationTrack.java
@@ -5,57 +5,42 @@ package org.geworkbench.util.sequences;
  * <p>Description: Modular Application Framework for Gene Expession, Sequence and Genotype Analysis</p>
  * <p>Copyright: Copyright (c) 2003 -2004</p>
  * <p>Company: Columbia University</p>
- *
  * @author not attributable
  * @version 1.0
  */
 
 public class SequenceAnnotationTrack {
 
-    String annotationName;
-    private int featureNum;
+    private AnnotationFeature first=null;
+    private AnnotationFeature last=null;
+    String annotationName=null;
+    private int featuresNum=0;
     private int color_num;
-    private int[] featureHitStart;
-    private int[] featureHitEnd;
-    private int[] sequenceHitStart;
-    private int[] sequenceHitEnd;
-    private int[] featureLength;
-    private int[] featureEnd;
-    private boolean[] featureDirection;
-    private boolean[] featureActive;
-    private double[] featureEValue;
-    private String[] featureName;
-    private String[] featureTag; /* or short name for displaying purpose */
-    private String[] featureURL;
 
     /**
      * Constructors
      */
 
     /**
-     * Use of this constructor will require separate use of initial function
-     */
+     *  Use of this constructor will require separate use of initial function
+     * */
     public SequenceAnnotationTrack() {
     }
 
-    public SequenceAnnotationTrack(int n) {
-        this.initSequenceAnnotationTrack(n);
-    }
+    private AnnotationFeature getFeatureByNum(int n){
+        AnnotationFeature sf=null;
+        sf=first;
+        int i=0;
 
-    public void initSequenceAnnotationTrack(int n) {
-        featureNum = n;
-        featureHitStart = new int[n];
-        featureHitEnd = new int[n];
-        sequenceHitStart = new int[n];
-        sequenceHitEnd = new int[n];
-        featureLength = new int[n];
-        featureEnd = new int[n];
-        featureDirection = new boolean[n];
-        featureActive = new boolean[n];
-        featureEValue = new double[n];
-        featureName = new String[n];
-        featureTag = new String[n];
-        featureURL = new String[n];
+        while(i<n){
+            if(sf!=last){
+                sf = (AnnotationFeature) sf.getNext();
+                i++;
+            } else {
+                return null;
+            }
+        }
+        return sf;
     }
 
     /**
@@ -66,106 +51,61 @@ public class SequenceAnnotationTrack {
     }
 
     public int getFeatureNum() {
-        return featureNum;
+        return featuresNum;
     }
 
     public int getColorNum() {
         return color_num;
     }
 
-    public int[] getFeatureHitStart() {
-        return featureHitStart;
-    }
-
     public int getFeatureHitStart(int n) {
-        return featureHitStart[n];
-    }
-
-    public int[] getFeatureHitEnd() {
-        return featureHitEnd;
+        return (getFeatureByNum(n)).getFeatureHitStart();
     }
 
     public int getFeatureHitEnd(int n) {
-        return featureHitEnd[n];
-    }
-
-    public int[] getSequenceHitStart() {
-        return sequenceHitStart;
+        return (getFeatureByNum(n)).getFeatureHitEnd();
     }
 
     public int getSequenceHitStart(int n) {
-        return sequenceHitStart[n];
-    }
-
-    public int[] getSequenceHitEnd() {
-        return sequenceHitEnd;
+        return (getFeatureByNum(n)).getSequenceHitStart();
     }
 
     public int getSequenceHitEnd(int n) {
-        return sequenceHitEnd[n];
-    }
-
-    public int[] getFeatureLength() {
-        return featureLength;
+        return (getFeatureByNum(n)).getSequenceHitEnd();
     }
 
     public int getFeatureLength(int n) {
-        return featureLength[n];
-    }
-
-    public boolean[] getFeatureDirection() {
-        return featureDirection;
+        return (getFeatureByNum(n)).getFeatureLength();
     }
 
     public boolean getFeatureDirection(int n) {
-        return featureDirection[n];
-    }
-
-    public boolean[] getFeatureActive() {
-        return featureActive;
+        return (getFeatureByNum(n)).getFeatureDirection();
     }
 
     public boolean getFeatureActive(int n) {
-        return featureActive[n];
-    }
-
-    public double[] getFeatureEValue() {
-        return featureEValue;
+        return (getFeatureByNum(n)).getFeatureActive();
     }
 
     public double getFeatureEValue(int n) {
-        return featureEValue[n];
-    }
-
-    public String[] getFeatureName() {
-        return featureName;
+        return (getFeatureByNum(n)).getFeatureEValue();
     }
 
     public String getFeatureName(int n) {
-        return featureName[n];
-    }
-
-    public String[] getFeatureTag() {
-        return featureTag;
+        return (getFeatureByNum(n)).getFeatureName();
     }
 
     public String getFeatureTag(int n) {
-        return featureTag[n];
-    }
-
-    public String[] getFeatureURL() {
-        return featureURL;
+        return (getFeatureByNum(n)).getFeatureTag();
     }
 
     public String getFeatureURL(int n) {
-        return featureURL[n];
+        if(n>=featuresNum)return null;
+        return (getFeatureByNum(n)).getFeatureURL();
     }
-
 
     /**
      * Modificators
      */
-
     public void setAnnotationName(String name) {
         annotationName = name;
     }
@@ -174,104 +114,76 @@ public class SequenceAnnotationTrack {
         color_num = cn;
     }
 
-    public void setFeatureNum(int fn) {
-        featureNum = fn;
-    }
-
-    public void setFeatureHitStart(int[] fs) {
-        featureHitStart = fs;
-    }
-
-    public void setFeatureHitStart(int n, int fs) {
-        featureHitStart[n] = fs;
-    }
-
-    public void setFeatureHitEnd(int[] fhe) {
-        featureHitEnd = fhe;
-    }
-
-    public void setFeatureHitEnd(int n, int fhe) {
-        featureHitEnd[n] = fhe;
-    }
-
-    public void setSequenceHitStart(int[] shs) {
-        sequenceHitStart = shs;
-    }
-
-    public void setSequenceHitStart(int n, int shs) {
-        sequenceHitStart[n] = shs;
-    }
-
-    public void setSequenceHitEnd(int[] she) {
-        sequenceHitEnd = she;
-    }
-
-    public void setSequenceHitEnd(int n, int she) {
-        sequenceHitEnd[n] = she;
-    }
-
-    public void setFeatureLength(int[] fl) {
-        featureLength = fl;
-    }
-
-    public void setFeatureLength(int n, int fl) {
-        featureLength[n] = fl;
-    }
-
-    public void setFeatureEnd(int[] fe) {
-        featureEnd = fe;
-    }
-
-    public void setFeatureEnd(int n, int fe) {
-        featureEnd[n] = fe;
-    }
-
-    public void setFeatureDirection(boolean[] fd) {
-        featureDirection = fd;
-    }
-
-    public void setFeatureDirection(int n, boolean fd) {
-        featureDirection[n] = fd;
-    }
-
-    public void setFeatureActive(boolean[] act) {
-        featureActive = act;
-    }
-
     public void setFeatureActive(int n, boolean act) {
-        featureActive[n] = act;
+        if(act){
+            (getFeatureByNum(n)).setActive();
+        } else {
+            (getFeatureByNum(n)).setInActive();
+        }
     }
 
-    public void setFeatureEValue(double[] fev) {
-        featureEValue = fev;
+    public void adjustFeaturePos(int n, int adj){
+        if(n>featuresNum)return;
+
+        getFeatureByNum(n).adjustPos(adj);
     }
 
-    public void setFeatureEValue(int n, double fev) {
-        featureEValue[n] = fev;
+    public void addFeature(int hit_ffrom, int hit_fto, int hit_sfrom, int hit_sto, int fl, boolean dir, boolean act, double
+                           fval, String fn, String ft, String furl ){
+        AnnotationFeature naf=new AnnotationFeature(last, null, hit_ffrom, hit_fto, hit_sfrom, hit_sto,
+                                                    fl, dir, act, fval, fn, ft, furl  );
+        if(last!=null){
+            last.setNext(naf);
+        }
+        last = naf;
+        if(first==null){
+            first = naf;
+        }
+        featuresNum++;
     }
 
-    public void setFeatureName(String[] fn) {
-        featureName = fn;
+    public void delFeatureByNum(int n){
+        if(n>featuresNum)return;
+
+        AnnotationFeature prev=(AnnotationFeature)(getFeatureByNum(n)).getPrevious();
+        AnnotationFeature nxt=(AnnotationFeature)(getFeatureByNum(n)).getNext();
+
+        if(prev==null){
+            if(nxt==null){
+                first=last=null;
+            }else{
+                nxt.setPrevious(null);
+            }
+        }else{
+            if(nxt==null){
+                prev.setNext(null);
+            } else {
+                prev.setNext(nxt);
+                nxt.setPrevious(prev);
+            }
+        }
+        featuresNum--;
     }
 
-    public void setFeatureName(int n, String fn) {
-        featureName[n] = fn;
+    public void delFeatureByNum(AnnotationFeature af){
+        AnnotationFeature prev=(AnnotationFeature)af.getPrevious();
+        AnnotationFeature nxt=(AnnotationFeature)af.getNext();
+
+        if(prev==null){
+            if(nxt==null){
+                first=last=null;
+            }else{
+                nxt.setPrevious(null);
+            }
+        }else{
+            if(nxt==null){
+                prev.setNext(null);
+            } else {
+                prev.setNext(nxt);
+                nxt.setPrevious(prev);
+            }
+        }
+        featuresNum--;
     }
 
-    public void setFeatureTag(String[] ft) {
-        featureTag = ft;
-    }
-
-    public void setFeatureTag(int n, String ft) {
-        featureTag[n] = ft;
-    }
-
-    public void setFeatureURL(String[] fu) {
-        featureURL = fu;
-    }
-
-    public void setFeatureURL(int n, String fu) {
-        featureURL[n] = fu;
-    }
 }
-
