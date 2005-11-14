fad2e222cf76be3a150295c41f54f89cdce1a896
John Watkinson <watkin@columbia.edu>
2005-11-14 20:17:21 +0000

    Added significance result sets for use by T-Test and others.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@820 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/bison/datastructure/bioobjects/microarray/CSSignificanceResultSet.java
@@ -0,0 +1,31 @@
+package org.geworkbench.bison.datastructure.bioobjects.microarray;
+
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+
+import java.io.File;
+
+/**
+ * @author John Watkinson
+ */
+public class CSSignificanceResultSet <T extends DSGeneMarker> extends CSAncillaryDataSet implements DSSignificanceResultSet<T> {
+
+    public CSSignificanceResultSet(DSMicroarraySet parent, String label) {
+        super(parent, label);
+    }
+
+    public File getDataSetFile() {
+        // not needed
+        return null;
+    }
+
+    public void setDataSetFile(File file) {
+        // no-op
+    }
+
+    public double getSignificance(T marker) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/org/geworkbench/bison/datastructure/bioobjects/microarray/DSSignificanceResultSet.java
@@ -0,0 +1,17 @@
+package org.geworkbench.bison.datastructure.bioobjects.microarray;
+
+import org.geworkbench.bison.datastructure.properties.DSNamed;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
+
+/**
+ * @author John Watkinson
+ */
+public interface DSSignificanceResultSet <T extends DSGeneMarker> extends DSAncillaryDataSet {
+
+    /**
+     * Gets the significance value for the marker.
+     */
+    public double getSignificance(T marker);
+
+}
