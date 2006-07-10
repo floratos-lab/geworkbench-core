c5cab7f441a77a07c5903ec8007677513253b941
Kiran Keshav <keshav@columbia.edu>
2006-07-10 12:09:23 +0000

    added a utility method for file copying
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@1629 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/Util.java
+++ src/org/geworkbench/util/Util.java
@@ -200,4 +200,18 @@ public class Util {
         }
         return name;
     }
+    
+    public static void copyFile(InputStream in, File dst) throws IOException {
+        //InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+    
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
 }
