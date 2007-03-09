3a137415b73e7be5935919a0cc49a4214a87a3aa
Xiaoqing Zhang <xiaoqing@columbia.edu>
2007-03-09 03:33:09 +0000

    fix bug 857
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2490 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/complex/pattern/SoapParmsDataSet.java
+++ src/org/geworkbench/bison/datastructure/complex/pattern/SoapParmsDataSet.java
@@ -95,26 +95,26 @@ public class SoapParmsDataSet extends CSAncillaryDataSet {
     }
 
     public boolean equals(Object ads) {
-        if (ads instanceof SoapParmsDataSet) {
-            SoapParmsDataSet pds = (SoapParmsDataSet) ads;
-            if (parms.getComputePValue() != pds.parms.getComputePValue()) return false;
-            if (parms.getCountSeq() != pds.parms.getCountSeq()) return false;
-            if (parms.getExact() != pds.parms.getExact()) return false;
-            if (parms.getExactTokens() != pds.parms.getExactTokens()) return false;
-            if (parms.getGroupingN() != pds.parms.getGroupingN()) return false;
-            if (parms.getGroupingType() != pds.parms.getGroupingType()) return false;
-            if (parms.getMaxPatternNo() != pds.parms.getMaxPatternNo()) return false;
-            if (parms.getMinPatternNo() != pds.parms.getMinPatternNo()) return false;
-            if (parms.getMinPer100Support() != pds.parms.getMinPer100Support()) return false;
-            if (parms.getMinPValue() != pds.parms.getMinPValue()) return false;
-            if (parms.getMinSupport() != pds.parms.getMinSupport()) return false;
-            if (parms.getMinTokens() != pds.parms.getMinTokens()) return false;
-            if (parms.getMinWTokens() != pds.parms.getMinWTokens()) return false;
-            if (parms.getSimilarityMatrix() != pds.parms.getSimilarityMatrix()) return false;
-            if (parms.getSimilarityThreshold() != pds.parms.getSimilarityThreshold()) return false;
-            if (parms.getWindow() != pds.parms.getWindow()) return false;
-            return true;
-        }
+//        if (ads instanceof SoapParmsDataSet) {
+//            SoapParmsDataSet pds = (SoapParmsDataSet) ads;
+//            if (parms.getComputePValue() != pds.parms.getComputePValue()) return false;
+//            if (parms.getCountSeq() != pds.parms.getCountSeq()) return false;
+//            if (parms.getExact() != pds.parms.getExact()) return false;
+//            if (parms.getExactTokens() != pds.parms.getExactTokens()) return false;
+//            if (parms.getGroupingN() != pds.parms.getGroupingN()) return false;
+//            if (parms.getGroupingType() != pds.parms.getGroupingType()) return false;
+//            if (parms.getMaxPatternNo() != pds.parms.getMaxPatternNo()) return false;
+//            if (parms.getMinPatternNo() != pds.parms.getMinPatternNo()) return false;
+//            if (parms.getMinPer100Support() != pds.parms.getMinPer100Support()) return false;
+//            if (parms.getMinPValue() != pds.parms.getMinPValue()) return false;
+//            if (parms.getMinSupport() != pds.parms.getMinSupport()) return false;
+//            if (parms.getMinTokens() != pds.parms.getMinTokens()) return false;
+//            if (parms.getMinWTokens() != pds.parms.getMinWTokens()) return false;
+//            if (parms.getSimilarityMatrix() != pds.parms.getSimilarityMatrix()) return false;
+//            if (parms.getSimilarityThreshold() != pds.parms.getSimilarityThreshold()) return false;
+//            if (parms.getWindow() != pds.parms.getWindow()) return false;
+//            return true;
+//        }
         return false;
     }
 
