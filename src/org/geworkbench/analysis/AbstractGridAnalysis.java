80b08fddc4e98106c19cce4961efbdeea76ffdd1
Kiran Keshav <keshav@columbia.edu>
2007-03-19 16:20:29 +0000

    an abstract grid analysis
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2573 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/analysis/AbstractGridAnalysis.java
@@ -0,0 +1,12 @@
+package org.geworkbench.analysis;
+
+/**
+ * Analyses that have a corresponding (ca)Grid component should extend this
+ * abstract class to have the grid analyses exposed.
+ * 
+ * @author keshav
+ * @version $Id: AbstractGridAnalysis.java,v 1.1 2007-03-19 16:20:29 keshav Exp $
+ */
+public abstract class AbstractGridAnalysis extends AbstractAnalysis {
+
+}
