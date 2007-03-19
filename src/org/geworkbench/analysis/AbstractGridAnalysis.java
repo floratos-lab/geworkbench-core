54eb42096ee8cc3c8df7df45e43e9fb6be396bde
Kiran Keshav <keshav@columbia.edu>
2007-03-19 18:13:37 +0000

    minor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2577 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/analysis/AbstractGridAnalysis.java
+++ src/org/geworkbench/analysis/AbstractGridAnalysis.java
@@ -1,12 +1,22 @@
 package org.geworkbench.analysis;
 
+import java.util.Map;
+
 /**
  * Analyses that have a corresponding (ca)Grid component should extend this
  * abstract class to have the grid analyses exposed.
  * 
  * @author keshav
- * @version $Id: AbstractGridAnalysis.java,v 1.1 2007-03-19 16:20:29 keshav Exp $
+ * @version $Id: AbstractGridAnalysis.java,v 1.2 2007-03-19 18:13:37 keshav Exp $
  */
 public abstract class AbstractGridAnalysis extends AbstractAnalysis {
+
+	/**
+	 * Returns the bison parameters, to be used for conversion in to cagrid
+	 * parameter types.
+	 * 
+	 * @return Map<String, String>
+	 */
+	public abstract Map<String, String> getBisonParameters();
 
 }
