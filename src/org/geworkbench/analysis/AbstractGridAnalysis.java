1c1f006b250eddf327329e3eafaa6b4d68baeb13
Kiran Keshav <keshav@columbia.edu>
2007-04-16 20:07:12 +0000

    added abstract method getAnalysisName to be implemented by grid analyses
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2691 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/analysis/AbstractGridAnalysis.java
+++ src/org/geworkbench/analysis/AbstractGridAnalysis.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * abstract class to have the grid analyses exposed.
  * 
  * @author keshav
- * @version $Id: AbstractGridAnalysis.java,v 1.3 2007-03-27 19:32:37 keshav Exp $
+ * @version $Id: AbstractGridAnalysis.java,v 1.4 2007-04-16 20:07:12 keshav Exp $
  */
 public abstract class AbstractGridAnalysis extends AbstractAnalysis {
 
@@ -16,5 +16,11 @@ public abstract class AbstractGridAnalysis extends AbstractAnalysis {
 	 * @return Map<String, Object>
 	 */
 	public abstract Map<String, Object> getBisonParameters();
+
+	/**
+	 * 
+	 * @return String
+	 */
+	public abstract String getAnalysisName();
 
 }
