c73e1b737817614da2405ab208462333af24895e
Kiran Keshav <keshav@columbia.edu>
2007-06-13 16:14:33 +0000

    minor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@2990 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/analysis/AbstractGridAnalysis.java
+++ src/org/geworkbench/analysis/AbstractGridAnalysis.java
@@ -7,17 +7,23 @@ import java.util.Map;
  * abstract class to have the grid analyses exposed.
  * 
  * @author keshav
- * @version $Id: AbstractGridAnalysis.java,v 1.4 2007-04-16 20:07:12 keshav Exp $
+ * @version $Id: AbstractGridAnalysis.java,v 1.5 2007-06-13 16:13:36 keshav Exp $
  */
 public abstract class AbstractGridAnalysis extends AbstractAnalysis {
 
 	/**
+	 * This method should be implmented to obtain the user input (which is
+	 * stored as BISON parameters), convert it to caGrid parameters, and return
+	 * a map with caGrid parameter values keyed by parameter names.
 	 * 
 	 * @return Map<String, Object>
 	 */
 	public abstract Map<String, Object> getBisonParameters();
 
 	/**
+	 * Analyses extending this class will implement this method, setting the
+	 * name of the service. This name should be equivalent to one of the names
+	 * in the "grid services cache". See {@link GridServiceRunner}.
 	 * 
 	 * @return String
 	 */
