c03809036ac30c10e4fd7ed71de6f25710caba7a
Kiran Keshav <keshav@columbia.edu>
2007-11-02 17:19:52 +0000

    serializable and formatted
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3310 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/views/DSDataSetView.java
+++ src/org/geworkbench/bison/datastructure/biocollections/views/DSDataSetView.java
@@ -1,5 +1,6 @@
 package org.geworkbench.bison.datastructure.biocollections.views;
 
+import java.io.Serializable;
 
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
@@ -7,68 +8,78 @@ import org.geworkbench.bison.datastructure.complex.panels.DSItemList;
 import org.geworkbench.bison.datastructure.complex.panels.DSPanel;
 
 /**
- * <p>Title: caWorkbench</p>
+ * <p>
+ * Title: caWorkbench
+ * </p>
  * <p/>
- * <p>Description: Modular Application Framework for Gene Expession, Sequence and Genotype
- * Analysis</p>
+ * <p>
+ * Description: Modular Application Framework for Gene Expession, Sequence and
+ * Genotype Analysis
+ * </p>
  * <p/>
- * <p>Copyright: Copyright (c) 2003 -2004</p>
+ * <p>
+ * Copyright: Copyright (c) 2003 -2004
+ * </p>
  * <p/>
- * <p>Company: Columbia University</p>
- *
- * Provides a view in to a {@link DSDataSet} and its associated {@link DSPanel DSPanels}.
- * The view can be optionally limited to exposing only those elements in active panels.
- *
+ * <p>
+ * Company: Columbia University
+ * </p>
+ * 
+ * Provides a view in to a {@link DSDataSet} and its associated
+ * {@link DSPanel DSPanels}. The view can be optionally limited to exposing
+ * only those elements in active panels.
+ * 
  * @author Adam Margolin
  * @version 3.0
  */
 
-public interface DSDataSetView <Q extends DSBioObject> {
+public interface DSDataSetView<Q extends DSBioObject> extends Serializable {
 
-    public int size();
+	public int size();
 
-    public Q get(int index);
+	public Q get(int index);
 
+	/**
+	 * @return A DSItemList containing all the <code>Q</code> type objects
+	 *         (generally microarrays) associated with this
+	 *         <code>DSDataView</code>.
+	 */
+	public DSItemList<Q> items();
 
-    /**
-     * @return A DSItemList containing all the <code>Q</code> type objects (generally microarrays)
-     *         associated with this <code>DSDataView</code>.
-     */
-    public DSItemList<Q> items();
+	/**
+	 * Set/reset item subselection based on activated panels.
+	 */
+	public void useItemPanel(boolean status);
 
-    /**
-     * Set/reset item subselection based on activated panels.
-     */
-    public void useItemPanel(boolean status);
+	/**
+	 * Gets the status of item activation.
+	 * 
+	 */
+	public boolean useItemPanel();
 
-    /**
-     * Gets the status of item activation.
-     *
-     */
-    public boolean useItemPanel();
+	/**
+	 * Assigns a specific item panel selection.
+	 */
+	public void setItemPanel(DSPanel<Q> mArrayPanel);
 
-    /**
-     * Assigns a specific item panel selection.
-     */
-    public void setItemPanel(DSPanel<Q> mArrayPanel);
+	/**
+	 * Assigns a specific item panel selection.
+	 */
+	public DSPanel<Q> getItemPanel();
 
-    /**
-     * Assigns a specific item panel selection.
-     */
-    public DSPanel<Q> getItemPanel();
+	/**
+	 * Sets the reference {@link DSDataSet} for this <code>DSDataSetView</code>.
+	 * 
+	 * @param dataSet
+	 *            The new reference dataset.
+	 */
+	public void setDataSet(DSDataSet<Q> dataSet);
 
-    /**
-     * Sets the reference {@link DSDataSet} for this <code>DSDataSetView</code>.
-     *
-     * @param dataSet The new reference dataset.
-     */
-    public void setDataSet(DSDataSet<Q> dataSet);
-
-    /**
-     * Get the <code>DSDataSet</code> object underlying this is view
-     *
-     * @return The reference <code>DSDataSet</code> object.
-     */
-    public DSDataSet<Q> getDataSet();
+	/**
+	 * Get the <code>DSDataSet</code> object underlying this is view
+	 * 
+	 * @return The reference <code>DSDataSet</code> object.
+	 */
+	public DSDataSet<Q> getDataSet();
 
 }
