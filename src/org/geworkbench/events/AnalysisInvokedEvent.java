eec04f64dc035cabf5a4231da95ee70ba1e92cee
sheths <sheths@columbia.edu>
2008-02-16 23:46:40 +0000

    genspace - getting the name of the dataset for the analysis run
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@3516 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/events/AnalysisInvokedEvent.java
+++ src/org/geworkbench/events/AnalysisInvokedEvent.java
@@ -2,21 +2,26 @@ package org.geworkbench.events;
 
 import org.geworkbench.engine.config.events.Event;
 
-
 /**
  * @author swapneelsheth
  */
 public class AnalysisInvokedEvent extends Event {
 	
 	private String analysisName;
+	private String dataSetName;
 	
-	public AnalysisInvokedEvent(String analysisName) {
+	public AnalysisInvokedEvent(String analysisName, String dataSetName) {
 		super(null);
 		this.analysisName = analysisName;
+		this.dataSetName = dataSetName;
 	}
 
 	public String getAnalysisName() {
 		return analysisName;
+	}
+
+	public String getDataSetName() {
+		return dataSetName;
 	}
 
 }
