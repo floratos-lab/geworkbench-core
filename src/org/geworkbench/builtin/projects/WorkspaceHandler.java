3b242bf2e65360335127b88d67b5e3c00864d0c9
Zhou Ji <zji@c2b2.columbia.edu>
2008-10-07 15:15:00 +0000

    cleanup
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4295 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/builtin/projects/WorkspaceHandler.java
+++ src/org/geworkbench/builtin/projects/WorkspaceHandler.java
@@ -1,35 +1,12 @@
-/*
- * The geworkbench-core project
- * 
- * Copyright (c) 2008 Columbia University
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package org.geworkbench.builtin.projects;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.concurrent.ExecutionException;
 
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JProgressBar;
 import javax.swing.filechooser.FileFilter;
 
 import org.apache.commons.logging.Log;
@@ -49,6 +26,7 @@ import org.geworkbench.util.threading.SwingWorker;
  * large ProjectPanel
  * 
  * @author zji
+ * @version $Id$
  * 
  */
 public class WorkspaceHandler {
