0c5a01449a347cfe444bb191062c9a8c9de46d33
Thomas Garben <tgarben@columbia.edu>
2009-02-10 19:49:51 +0000

    WIP
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4800 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/engine/test/ComponentClassloaderTest.java
+++ src/org/geworkbench/engine/test/ComponentClassloaderTest.java
@@ -1,8 +1,13 @@
 package org.geworkbench.engine.test;
 
 import junit.framework.TestCase;
+
+import org.apache.commons.digester.Digester;
+import org.geworkbench.builtin.ComponentConfigurationManager;
+import org.geworkbench.engine.config.rules.PluginObject;
 import org.geworkbench.engine.management.ComponentResource;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -16,4 +21,33 @@ public class ComponentClassloaderTest extends TestCase {
         ComponentResource cr = new ComponentResource("gears", false);
         ClassLoader loader = cr.getClassLoader();
     }
+        
+    public void testLoadCmmXmlFile() throws IOException {
+
+    	String componentsDir = "c:\\unittest\\HierClusterViewAppComponent.ccm.xml";
+		File dir = new File(componentsDir);
+		
+		if (!dir.isDirectory()){
+			throw new RuntimeException();
+		}
+    }	
+    
+    public void testConfigureDigester(){
+
+    	Digester digester = new Digester(new org.apache.xerces.parsers.SAXParser());
+    	
+    	ComponentConfigurationManager ccm = new ComponentConfigurationManager();
+    	ccm.configure (digester);
+    }
+    
+    public void testParseCmmXmlFile() throws IOException {
+    	String componentsDir = "c:\\unittest\\";
+    	
+    	ComponentConfigurationManager ccm = new ComponentConfigurationManager(componentsDir); 
+
+    	ComponentResource componentResource = new ComponentResource("gears", false); 
+    	ccm.parseComponentDescriptor(componentResource);
+    }	
+    
+    
 }
