2c075c2b33f819981df7af199653fbf01335df5d
Thomas Garben <tgarben@columbia.edu>
2009-02-10 23:06:29 +0000

    WIP
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4802 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/builtin/ComponentConfigurationManager.java
+++ src/org/geworkbench/builtin/ComponentConfigurationManager.java
@@ -2,24 +2,16 @@ package org.geworkbench.builtin;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
-
-import javax.swing.JOptionPane;
 
 import org.apache.commons.digester.Digester;
-import org.apache.commons.digester.ObjectCreateRule;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.geworkbench.engine.config.rules.PluginObject;
 import org.geworkbench.engine.config.rules.PluginRule;
-import org.geworkbench.engine.config.rules.PluginRuleCCM;
 import org.geworkbench.engine.management.ComponentResource;
 import org.xml.sax.SAXException;
-import org.xml.sax.Attributes;
 
 /**
  * Manages the dynamic loading and removal of components.
@@ -34,10 +26,6 @@ public class ComponentConfigurationManager {
 	private Log log = LogFactory.getLog(this.getClass());
 	private String[] files = null;
 	private Digester digester = null;
-
-	public ComponentConfigurationManager() {
-		super();
-	}
 
 	/**
 	 * 
@@ -58,16 +46,16 @@ public class ComponentConfigurationManager {
 	 * 
 	 * @param resource
 	 */
-	private ComponentResource initializeComponentResource(String resourceName) {
+	private ComponentResource initializeComponentResource(String resource) {
 
-		if (StringUtils.isEmpty(resourceName)) {
+		if (StringUtils.isEmpty(resource)) {
 			log.error("Input resource is null.  Returning ...");
 		}
 		List<String> list = Arrays.asList(files);
 
 		ComponentResource componentResource = null;
-		if (list.contains(resourceName)) {
-			int index = list.indexOf(resourceName);
+		if (list.contains(resource)) {
+			int index = list.indexOf(resource);
 			File file = new File(list.get(index));
 			try {
 				componentResource = new ComponentResource(file.getPath(), false);
@@ -78,7 +66,7 @@ public class ComponentConfigurationManager {
 			}
 			return componentResource;
 		}
-		log.error("Resource does not exist for " + resourceName
+		log.error("Resource does not exist for " + resource
 				+ ".  Returning null.");
 		return null;
 	}
@@ -102,50 +90,20 @@ public class ComponentConfigurationManager {
 	 * @throws IOException
 	 */
 	public Object parseComponentDescriptor(ComponentResource componentResource) {
-		// TODO implement me (see how this is currently done in PluginObject's
-		// processComponentDescriptor method
+		// TODO change the xml rules so we are using cwb.xml tags.
+		digester.addRule("geaw-config/plugin", new PluginRule(
+				"org.geworkbench.engine.config.rules.PluginObject"));
 
-		String componentName = componentResource.getName();
-		String componentPath = componentResource.getDir();
-		
-		String configFileArg = null;
-        String configFileName = null;
-		
-	    Digester ccmDigester = new Digester(new org.apache.xerces.parsers.SAXParser());
-		configure(ccmDigester);
-		
+		String dir = componentResource.getDir();
+		File f = new File(dir);
 
-            InputStream inputStream = null;
-			try {
-				inputStream = Class.forName("org.geworkbench.engine.config.PluginLoader").getResourceAsStream("/" + configFileName);
-	            if (inputStream == null) {
-//	                exitOnErrorMessage("Invalid or absent configuration file.");
-					throw new RuntimeException(
-							"Invalid or absent configuration file. "
-									+ componentResource.getName());
-	            }
-			} catch (ClassNotFoundException e) {
-				log.error(e, e);
-				throw new RuntimeException(
-						"ClassNotFoundException: Error parsing component descriptor for component resource "
-								+ componentResource.getName());
-			}
-            
-        Object parsedObject = null;
-            
-        try {
-			parsedObject = ccmDigester.parse(inputStream);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		Object parsedObject = null;
+		try {
+			parsedObject = digester.parse(f);
+		} catch (Exception e) {
 			log.error(e, e);
 			throw new RuntimeException(
-					"IOException: Error parsing component descriptor for component resource "
-							+ componentResource.getName());
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			log.error(e, e);
-			throw new RuntimeException(
-					"SAXException: Error parsing component descriptor for component resource "
+					"Error parsing component descriptor for component resource "
 							+ componentResource.getName());
 		}
 
@@ -167,90 +125,4 @@ public class ComponentConfigurationManager {
 
 	}
 
-	
-    
-    /**
-     * Configure the rules for translating the application configuration file.
-     */
-    public Digester configure(Digester ccmDigester) {
-        ccmDigester.setUseContextClassLoader(true);
-        
-        // Instantiates a plugin and adds it in the PluginResgistry
-        ccmDigester.addRule("plugin", new PluginRuleCCMSimulator("org.geworkbench.engine.config.rules.PluginObject"));
-        
-        ccmDigester.addCallMethod("plugin/gui-area", "addGUIComponent", 1);
-        ccmDigester.addCallParam("plugin/gui-area", 0, "name");
-        
-        return ccmDigester;
-        
-    }
-
-    
-    public class PluginRuleCCMSimulator extends ObjectCreateRule {
-
-    	Vector pluginObjects = new Vector(); // but this isn't static??????
-    	
-		public PluginRuleCCMSimulator(String className) {
-			super(className);
-		}
-    	
-	    public void begin(String namespace, String name, Attributes attributes) throws Exception {
-	        PluginObject pginObj;
-	        super.begin(namespace, name, attributes);
-	        pginObj = (PluginObject) super.getDigester().peek();
-	        pluginObjects.add(pginObj);
-	        // We need to instantiate the plugin descriptor before the various
-	        // CallMethod rules are called.
-	        pginObj.createCCMPlugin(attributes.getValue("id"), attributes.getValue("name"), attributes.getValue("class"), attributes.getValue("source"));
-	    }
-
-	    /**
-	     * Overrides the corresponding method from <code>ObjectCreateRule</code>.
-	     * Called at the end of parsing, in order to finish up. It invokes the
-	     * {@link org.geworkbench.engine.config.rules.PluginObject#finish finish} method of each
-	     * <code>PluginObject</code> generated through the parsing of the
-	     * application configuration file.
-	     *
-	     * @throws Exception
-	     */
-	    public void finish() throws Exception {
-	        int size = pluginObjects.size();
-	        for (int i = 0; i < size; ++i)
-	            ((PluginObject) pluginObjects.get(i)).finish();
-	    }
-
-    }
-
-    
-    
-    public class PluginObjectSimulator{
-    	
-        /**
-         * Creates a new plugin in and adds it to the <code>PluginRegistry</code>.
-         * The plugin is instantiated by a call to its default constructor.
-         *
-         * @param id           Id to be used for the new plugin.
-         * @param name         Name to be used for the new plugin.
-         * @param className    Class name for the new plugin.
-         * @param resourceName Resource from which to load the plugin.
-         */
-        public void createCcmPlugin(String id, String name, String className, String resourceName) {
-
-        }
-
-    	public ComponentResource initializeComponentResource(String resourceName) {
-
-    		return null;
-    	}
-        
-    	
-    }
-    
-    
-    
-    private void exitOnErrorMessage(String message) {
-        JOptionPane.showMessageDialog(null, message, "Plugin Load Error", JOptionPane.ERROR_MESSAGE);
-    }
-
-	
 }
--- src/org/geworkbench/engine/test/ComponentClassloaderTest.java
+++ src/org/geworkbench/engine/test/ComponentClassloaderTest.java
@@ -4,11 +4,15 @@ import junit.framework.TestCase;
 
 import org.apache.commons.digester.Digester;
 import org.geworkbench.builtin.ComponentConfigurationManager;
+import org.geworkbench.engine.config.UILauncher;
 import org.geworkbench.engine.config.rules.PluginObject;
+import org.geworkbench.engine.config.rules.PluginRule;
 import org.geworkbench.engine.management.ComponentResource;
+import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * User: matt
@@ -22,22 +26,39 @@ public class ComponentClassloaderTest extends TestCase {
         ClassLoader loader = cr.getClassLoader();
     }
         
-    public void testLoadCmmXmlFile() throws IOException {
+    public void testLoadCmmXmlFile() throws ClassNotFoundException, IOException, SAXException  {
+    	Digester emptyDigester = new Digester(new org.apache.xerces.parsers.SAXParser());
+    	
+    	String componentDir = System.getProperty(UILauncher.COMPONENTS_DIR_PROPERTY);
+    	
+        if (componentDir == null) {
+            componentDir = UILauncher.DEFAULT_COMPONENTS_DIR;
+        }
 
-    	String componentsDir = "c:\\unittest\\HierClusterViewAppComponent.ccm.xml";
-		File dir = new File(componentsDir);
+        componentDir += "\\HierClusterViewAppComponent\\";
+    	String ccmFileName = "HierClusterViewAppComponent.ccm.xml";
 		
-		if (!dir.isDirectory()){
-			throw new RuntimeException();
-		}
+    	InputStream inputStream = null;
+
+    	Class localClass  = Class.forName("org.geworkbench.engine.test.ComponentClassloaderTest");
+    	inputStream = localClass.getResourceAsStream("/" + ccmFileName);
+		
+		emptyDigester.parse(inputStream);
     }	
     
-    public void testConfigureDigester(){
+    public void testDigesterParse(){
 
     	Digester digester = new Digester(new org.apache.xerces.parsers.SAXParser());
     	
-    	ComponentConfigurationManager ccm = new ComponentConfigurationManager();
-    	ccm.configure (digester);
+    	digester.setUseContextClassLoader(true);
+        
+        // Instantiates a plugin and adds it in the PluginResgistry
+    	digester.addRule("plugin", new PluginRule("org.geworkbench.engine.config.rules.PluginObject"));
+        
+    	digester.addCallMethod("plugin/gui-area", "addGUIComponent", 1);
+    	digester.addCallParam("plugin/gui-area", 0, "name");
+
+//    	digester.parse(inputStream);
     }
     
     public void testParseCmmXmlFile() throws IOException {
