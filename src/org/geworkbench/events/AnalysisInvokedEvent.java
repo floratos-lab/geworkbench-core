d950bff9e20c445d65e23f5eb8e22e20b8961549
Yih-Shien Chiang <chiangy@columbia.edu>
2009-02-23 18:08:49 +0000

    genSpace: changed AnalysisInvokedEvent to accept an Analysis.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@4851 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/events/AnalysisInvokedEvent.java
+++ src/org/geworkbench/events/AnalysisInvokedEvent.java
@@ -1,25 +1,40 @@
 package org.geworkbench.events;
 
+import org.geworkbench.bison.model.analysis.Analysis;
 import org.geworkbench.engine.config.events.Event;
 
 /**
  * @author swapneelsheth
+ * @version $Id: AnalysisInvokedEvent.java,v 1.3 2009-02-23 18:08:48 chiangy Exp $
  */
 public class AnalysisInvokedEvent extends Event {
-	
-	private String analysisName;
+
+	private Analysis analysis;
 	private String dataSetName;
-	
-	public AnalysisInvokedEvent(String analysisName, String dataSetName) {
+
+	/**
+	 * 
+	 * @param analysis
+	 * @param dataSetName
+	 */
+	public AnalysisInvokedEvent(Analysis analysis, String dataSetName) {
 		super(null);
-		this.analysisName = analysisName;
+		this.analysis = analysis;
 		this.dataSetName = dataSetName;
 	}
 
-	public String getAnalysisName() {
-		return analysisName;
+	/**
+	 * 
+	 * @return
+	 */
+	public Analysis getAnalysis() {
+		return analysis;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getDataSetName() {
 		return dataSetName;
 	}
