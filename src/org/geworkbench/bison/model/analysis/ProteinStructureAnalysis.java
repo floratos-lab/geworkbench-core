9664622d4d8e4d0d1370223f5aba78a237aaaae0
Zhou Ji <zji@c2b2.columbia.edu>
2009-05-12 18:53:31 +0000

    renaming interface
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5111 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/model/analysis/ProteinStructureAnalysis.java
+++ src/org/geworkbench/bison/model/analysis/ProteinStructureAnalysis.java
@@ -1,7 +1,11 @@
 package org.geworkbench.bison.model.analysis;
 
 /**
- * Tagging interface for protein structure analysis algorithms(skyline)
+ * The interface to identify protein structure analysis, which takes PDB files as input data.
+ * This currently includesMarkUs and SkyLine.
+ * 
+ * @author mengw, zji
+ * @version $Id: ProteinStructureAnalysis.java,v 1.2 2009-05-12 18:53:31 jiz Exp $
  */
 public interface ProteinStructureAnalysis extends Analysis {
 }
