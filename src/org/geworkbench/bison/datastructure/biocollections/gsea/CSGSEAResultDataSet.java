fd1e427711fe430f7d744e1b97133e97c3e48922
Marc-Danie Nazaire <nazaire@broadinstitute.org>
2009-07-07 20:28:55 +0000

    adding class to hold genepattern gsea results
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5383 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/bison/datastructure/biocollections/gsea/CSGSEAResultDataSet.java
@@ -0,0 +1,106 @@
+package org.geworkbench.bison.datastructure.biocollections.gsea;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nazaire
+ * Date: Jul 7, 2009
+ */
+public class CSGSEAResultDataSet extends CSAncillaryDataSet implements DSGSEAResultDataSet {
+
+	private static final long serialVersionUID = 1L;
+
+	static Log log = LogFactory.getLog(CSGSEAResultDataSet.class);
+	private String reportFile;
+	private String gsDatabaseFile;
+
+
+	public CSGSEAResultDataSet(DSDataSet parent, String label, String reportFile,
+			String gsDatabaseFile) {
+		super(parent, label);
+		this.reportFile = reportFile;
+		this.gsDatabaseFile = gsDatabaseFile;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet#getDataSetFile()
+	 */
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.pca.DSPCADataSet#getDataSetFile()
+	 */
+	public File getDataSetFile() {
+		// no-op
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet#setDataSetFile(java.io.File)
+	 */
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.gsea.DSGSEAResultDataSet#setDataSetFile(java.io.File)
+	 */
+	public void setDataSetFile(File file) {
+		// no-op
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.gsea.DSGSEAResultDataSet#getReportFile()
+	 */
+	public String getReportFile()
+    {
+		return reportFile;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.gsea.DSGSEAResultDataSet#getGSDatabaseFile()
+	 */
+	public String getGSDatabaseFile()
+    {
+		return gsDatabaseFile;
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.gsea.DSGSEAResultDataSet#writeToFile(java.lang.String)
+	 */
+	public void writeToFile(String fileName)
+    {
+		File file = new File(fileName);
+
+		try
+        {
+			file.createNewFile();
+			if (!file.canWrite())
+            {
+				JOptionPane.showMessageDialog(null,
+						"Cannot write to specified file.");
+				return;
+			}
+		} catch (IOException io) {
+			log.error(io);
+		}
+	}
+}
+
---
+++ src/org/geworkbench/bison/datastructure/biocollections/gsea/DSGSEAResultDataSet.java
@@ -0,0 +1,46 @@
+package org.geworkbench.bison.datastructure.biocollections.gsea;
+
+import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
+
+import java.io.File;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nazaire
+ * Date: Jul 7, 2009
+ */
+public interface DSGSEAResultDataSet extends DSAncillaryDataSet 
+{
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet#getDataSetFile()
+	 */
+	public abstract File getDataSetFile();
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet#setDataSetFile(java.io.File)
+	 */
+	public abstract void setDataSetFile(File file);
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.geworkbench.bison.datastructure.biocollections.DSDataSet#writeToFile(java.lang.String)
+	 */
+	public abstract void writeToFile(String fileName);
+
+	/**
+	 *
+	 * @return
+	 */
+	public abstract String getReportFile();
+
+	/**
+	 *
+	 * @return
+	 */
+	public abstract String getGSDatabaseFile();
+}
