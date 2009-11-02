38b55384861889768493e4a2e5253f98009b8c5e
Zhou Ji <zji@c2b2.columbia.edu>
2009-11-02 22:42:08 +0000

    pure cleaning-up: no logic/functionality change
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5729 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/APSerializable.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/APSerializable.java
@@ -3,6 +3,7 @@ package org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser;
 import org.apache.commons.collections15.MultiMap;
 import org.apache.commons.collections15.map.ListOrderedMap;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -12,31 +13,37 @@ import java.util.Vector;
 
 /**
  * @author John Watkinson
+ * @version $Id: APSerializable.java,v 1.2 2009-11-02 22:42:08 jiz Exp $
  */
 public class APSerializable implements Serializable {
+	private static final long serialVersionUID = 6455427625940524515L;
 
-    DSDataSet currentDataSet = null;
-    Map<DSDataSet, String> datasetToChipTypes = new HashMap<DSDataSet, String>();
-    Map<String, ListOrderedMap<String, Map<String, String>>> chipTypeToAnnotations = new HashMap<String, ListOrderedMap<String, Map<String, String>>>();
+	DSDataSet<? extends DSBioObject> currentDataSet = null;
+	Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes = new HashMap<DSDataSet<? extends DSBioObject>, String>();
+	Map<String, ListOrderedMap<String, Map<String, String>>> chipTypeToAnnotations = new HashMap<String, ListOrderedMap<String, Map<String, String>>>();
 
-    HashMap<DSDataSet, AnnotationParser.CustomAnnotations> customAnnotations = new HashMap<DSDataSet, AnnotationParser.CustomAnnotations>();
+	HashMap<String, String> chiptypeMap = new HashMap<String, String>();
 
-    HashMap chiptypeMap = new HashMap();
+	Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap = new HashMap<String, ListOrderedMap<String, Vector<String>>>();
+	ArrayList<String> chipTypes = new ArrayList<String>();
+	MultiMap<String, String> GOIDToAffy = null;
+	MultiMap<String, String> affyToGOID = null;
 
-    Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap = new HashMap<String, ListOrderedMap<String, Vector<String>>>();
-    ArrayList<String> chipTypes = new ArrayList<String>();
-    MultiMap<String, String> GOIDToAffy = null;
-    MultiMap<String, String> affyToGOID = null;
-
-    public APSerializable(DSDataSet currentDataSet, Map<DSDataSet, String> datasetToChipTypes, Map<String, ListOrderedMap<String, Map<String, String>>> chipTypeToAnnotations, HashMap<DSDataSet, AnnotationParser.CustomAnnotations> customAnnotations, HashMap chiptypeMap, Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap, ArrayList<String> chipTypes, MultiMap<String, String> GOIDToAffy, MultiMap<String, String> affyToGOID) {
-        this.currentDataSet = currentDataSet;
-        this.datasetToChipTypes = datasetToChipTypes;
-        this.chipTypeToAnnotations = chipTypeToAnnotations;
-        this.customAnnotations = customAnnotations;
-        this.chiptypeMap = chiptypeMap;
-        this.geneNameMap = geneNameMap;
-        this.chipTypes = chipTypes;
-        this.GOIDToAffy = GOIDToAffy;
-        this.affyToGOID = affyToGOID;
-    }
+	public APSerializable(
+			DSDataSet<? extends DSBioObject> currentDataSet2,
+			Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes2,
+			Map<String, ListOrderedMap<String, Map<String, String>>> chipTypeToAnnotations,
+			HashMap<String, String> chiptypeMap,
+			Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap,
+			ArrayList<String> chipTypes, MultiMap<String, String> GOIDToAffy,
+			MultiMap<String, String> affyToGOID) {
+		this.currentDataSet = currentDataSet2;
+		this.datasetToChipTypes = datasetToChipTypes2;
+		this.chipTypeToAnnotations = chipTypeToAnnotations;
+		this.chiptypeMap = chiptypeMap;
+		this.geneNameMap = geneNameMap;
+		this.chipTypes = chipTypes;
+		this.GOIDToAffy = GOIDToAffy;
+		this.affyToGOID = affyToGOID;
+	}
 }
--- src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/AnnotationParser.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/AnnotationParser.java
@@ -10,7 +10,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
@@ -18,7 +17,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
@@ -35,41 +33,33 @@ import javax.swing.event.HyperlinkListener;
 
 import org.apache.commons.collections15.MultiMap;
 import org.apache.commons.collections15.map.ListOrderedMap;
-import org.apache.commons.collections15.multimap.MultiHashMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.biocollections.microarrays.CSMicroarraySet;
-import org.geworkbench.bison.util.RandomNumberGenerator;
+import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 import org.geworkbench.engine.preferences.PreferencesManager;
 import org.geworkbench.engine.properties.PropertiesManager;
-import org.geworkbench.util.annotation.Gene;
 
 import com.Ostermiller.util.CSVParser;
 import com.Ostermiller.util.LabeledCSVParser;
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 
 /**
- * <p>
- * Title: caWorkbench 3.0
- * </p>
- * <p>
- * Description:This Class is for retrieving probe annotation information from
- * default annotation files provided by Affymetrix
- * </p>
  * 
- * @author Xuegong Wang, manjunath at genomecenter dot columbia dot edu
- * @version 1.5
+ * Description:This Class is for retrieving probe annotation information from
+ * default annotation files provided by Affymetrix.
+ * 
+ * @author Xuegong Wang
+ * @author manjunath at genomecenter dot columbia dot edu
+ * @version $Id: AnnotationParser.java,v 1.40 2009-11-02 22:42:08 jiz Exp $
  */
 
 public class AnnotationParser implements Serializable {
+	private static final long serialVersionUID = -117234619759135916L;
 
 	static Log log = LogFactory.getLog(AnnotationParser.class);
 
-	public static final String version = "31";
-
-	// when you change file format etc. this version number need to be changed
-	// so that the old file will be deleted.
 	static int counter = 0;
 
 	public static final String GENE_ONTOLOGY_BIOLOGICAL_PROCESS = "Gene Ontology Biological Process";
@@ -97,7 +87,6 @@ public class AnnotationParser implements Serializable {
 
 	public static final String UNIGENE_CLUSTER = "Archival UniGene Cluster";
 
-	// Todo figure out where locus link info comes from
 	public static final String LOCUSLINK = "Entrez Gene"; // LocusLink
 
 	public static final String SWISSPROT = "SwissProt"; // swissprot
@@ -123,16 +112,14 @@ public class AnnotationParser implements Serializable {
 			GENE_ONTOLOGY_BIOLOGICAL_PROCESS, GENE_ONTOLOGY_CELLULAR_COMPONENT,
 			GENE_ONTOLOGY_MOLECULAR_FUNCTION, PATHWAY, TRANSCRIPT };
 
-	// // FIELDS
-	private static DSDataSet currentDataSet = null;
+	// FIELDS
+	private static DSDataSet<? extends DSBioObject> currentDataSet = null;
 
-	private static Map<DSDataSet, String> datasetToChipTypes = new HashMap<DSDataSet, String>();
+	private static Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes = new HashMap<DSDataSet<? extends DSBioObject>, String>();
 
 	private static Map<String, ListOrderedMap<String, Map<String, String>>> chipTypeToAnnotations = new HashMap<String, ListOrderedMap<String, Map<String, String>>>();
 
-	private static HashMap<DSDataSet, CustomAnnotations> customAnnotations = new HashMap<DSDataSet, CustomAnnotations>();
-
-	public static HashMap chiptypeMap = new HashMap();
+	public static HashMap<String, String> chiptypeMap = new HashMap<String, String>();
 
 	public static Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap = new HashMap<String, ListOrderedMap<String, Vector<String>>>();
 
@@ -141,12 +128,11 @@ public class AnnotationParser implements Serializable {
 	static MultiMap<String, String> GOIDToAffy = null;
 
 	static MultiMap<String, String> affyToGOID = null;
-
-	// // END FIELDS
+	// END FIELDS
 
 	public static APSerializable getSerializable() {
 		return new APSerializable(currentDataSet, datasetToChipTypes,
-				chipTypeToAnnotations, customAnnotations, chiptypeMap,
+				chipTypeToAnnotations, chiptypeMap,
 				geneNameMap, chipTypes, GOIDToAffy, affyToGOID);
 	}
 
@@ -154,7 +140,6 @@ public class AnnotationParser implements Serializable {
 		currentDataSet = aps.currentDataSet;
 		datasetToChipTypes = aps.datasetToChipTypes;
 		chipTypeToAnnotations = aps.chipTypeToAnnotations;
-		customAnnotations = aps.customAnnotations;
 		chiptypeMap = aps.chiptypeMap;
 		geneNameMap = aps.geneNameMap;
 		chipTypes = aps.chipTypes;
@@ -198,26 +183,16 @@ public class AnnotationParser implements Serializable {
 				str = br.readLine();
 			}
 			br.close();
-			File temp = new File(tmpDir + chiptyemapfilename);
-			if (temp.exists()) {
-				BufferedReader br2 = new BufferedReader(new FileReader(temp));
-				str = br2.readLine();
-				while (str != null) {
-					String[] data = str.split(",");
-					str = br2.readLine();
-				}
-				br2.close();
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static DSDataSet getCurrentDataSet() {
+	public static DSDataSet<? extends DSBioObject> getCurrentDataSet() {
 		return currentDataSet;
 	}
 
-	public static void setCurrentDataSet(DSDataSet currentDataSet) {
+	public static void setCurrentDataSet(DSDataSet<DSBioObject> currentDataSet) {
 		AnnotationParser.currentDataSet = currentDataSet;
 	}
 
@@ -229,11 +204,11 @@ public class AnnotationParser implements Serializable {
 		}
 	}
 
-	public static String getChipType(DSDataSet dataset) {
+	public static String getChipType(DSDataSet<? extends DSBioObject> dataset) {
 		return datasetToChipTypes.get(dataset);
 	}
 
-	public static boolean setChipType(DSDataSet dataset, String chiptype) {
+	public static boolean setChipType(DSDataSet<? extends DSBioObject> dataset, String chiptype) {
 		datasetToChipTypes.put(dataset, chiptype);
 		currentDataSet = dataset;
 		if (chiptypeMap.containsValue(chiptype)) {
@@ -243,7 +218,7 @@ public class AnnotationParser implements Serializable {
 		return false;
 	}
 
-	public static boolean setChipType(DSDataSet dataset, String chiptype,
+	public static boolean setChipType(DSDataSet<? extends DSBioObject> dataset, String chiptype,
 			File annotationData) {
 		datasetToChipTypes.put(dataset, chiptype);
 		currentDataSet = dataset;
@@ -309,7 +284,6 @@ public class AnnotationParser implements Serializable {
 					fis.close();
 					bis.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
@@ -342,13 +316,6 @@ public class AnnotationParser implements Serializable {
 				}
 			}
 		}
-	}
-
-	private static File createFilewithID() {
-
-		String tempString = "annotationParser" + RandomNumberGenerator.getID()
-				+ ".idx";
-		return new File(tmpDir + tempString);
 	}
 
 	public static String getGeneName(String id) {
@@ -413,69 +380,6 @@ public class AnnotationParser implements Serializable {
 		return info;
 	}
 
-	// used to parse info from raw go data
-	private static String parseGo(String godata) {
-		String result = "";
-		String[] gos = godata.split(MAIN_DELIMITER);
-
-		for (int i = 0; i < gos.length; i++) {
-			String onego = gos[i];
-			String[] gocat = onego.split("//");
-			if (gocat.length > 1) {
-				int k = Integer.parseInt(gocat[0].trim()) + 10000000;
-				gocat[0] = Integer.toString(k).substring(1);
-				result = new String(result + "GO:" + gocat[0] + "::"
-						+ gocat[1].trim() + "\t");
-			}
-		}
-		return result;
-	}
-
-	public static MultiMap<String, String> getGotable() {
-		if (GOIDToAffy != null) {
-			return GOIDToAffy;
-		}
-		try {
-			ListOrderedMap<String, Map<String, String>> annots = getAllAnnotationsForDataSet(currentDataSet);
-			GOIDToAffy = new MultiHashMap<String, String>();
-			affyToGOID = new MultiHashMap<String, String>();
-			for (String marker : annots.keySet()) {
-				marker = marker.trim();
-				log.debug("Adding go terms for marker " + marker);
-				String bio = annots.get(marker).get(
-						GENE_ONTOLOGY_BIOLOGICAL_PROCESS);
-				Vector<String> goIds = getGOIds(bio);
-				for (int i = 0; i < goIds.size(); i++) {
-					String goId = goIds.elementAt(i).trim();
-					affyToGOID.put(marker, goId);
-					GOIDToAffy.put(goId, marker);
-				}
-				String cell = annots.get(marker).get(
-						GENE_ONTOLOGY_CELLULAR_COMPONENT);
-				Vector<String> cellIds = getGOIds(cell);
-				for (int i = 0; i < cellIds.size(); i++) {
-					String goId = cellIds.elementAt(i).trim();
-					affyToGOID.put(marker, goId);
-					GOIDToAffy.put(goId, marker);
-				}
-				String molecular = annots.get(marker).get(
-						GENE_ONTOLOGY_MOLECULAR_FUNCTION);
-				Vector<String> molIds = getGOIds(molecular);
-				for (int i = 0; i < molIds.size(); i++) {
-					String goId = molIds.elementAt(i).trim();
-					affyToGOID.put(marker, goId);
-					GOIDToAffy.put(goId, marker);
-				}
-			}
-			// todo: Make this return the correct map, I think we need
-			// affyToGOID here
-			return GOIDToAffy;
-		} catch (Exception e) {
-			log.warn("Problem getting go table.", e);
-			return null;
-		}
-	}
-
 	public static Set<String> getSwissProtIDs(String markerID) {
 		ListOrderedMap<String, Map<String, String>> annots = getAllAnnotationsForDataSet(currentDataSet);
 		String annot = annots.get(markerID).get(SWISSPROT);
@@ -490,34 +394,14 @@ public class AnnotationParser implements Serializable {
 	}
 
 	public static ListOrderedMap<String, Map<String, String>> getAllAnnotationsForDataSet(
-			DSDataSet dataset) {
+			DSDataSet<? extends DSBioObject> dataset) {
 		String chipType = datasetToChipTypes.get(dataset);
 		ListOrderedMap<String, Map<String, String>> annots = chipTypeToAnnotations
 				.get(chipType);
 		return annots;
 	}
 
-	public static Set<String> getAffyIds() {
-		try {
-			return getAllAnnotationsForDataSet(currentDataSet).keySet();
-		} catch (Exception e) {
-			log.warn("Problem getting current affy IDs.", e);
-			return null;
-		}
-	}
-
-	private static Vector<String> getGOIds(String goString) {
-		Vector<String> goIds = new Vector<String>();
-		String[] blocks = goString.split(MAIN_DELIMITER);
-		for (int i = 0; i < blocks.length; i++) {
-			String block = blocks[i];
-			String id = block.split("//")[0];
-			goIds.add(id);
-		}
-		return goIds;
-	}
-
-	public static String matchChipType(DSDataSet dataset, String id,
+	public static String matchChipType(DSDataSet<? extends DSBioObject> dataset, String id,
 			boolean askIfNotFound) {
 		PreferencesManager preferencesManager = PreferencesManager
 				.getPreferencesManager();
@@ -597,9 +481,9 @@ public class AnnotationParser implements Serializable {
 		String osName = System.getProperty("os.name");
 		try {
 			if (osName.startsWith("Mac OS")) {
-				Class fileMgr = Class.forName("com.apple.eio.FileManager");
+				Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
 				Method openURL = fileMgr.getDeclaredMethod("openURL",
-						new Class[] { String.class });
+						new Class<?>[] { String.class });
 				openURL.invoke(null, new Object[] { url });
 			} else if (osName.startsWith("Windows")) {
 				Runtime.getRuntime().exec(
@@ -624,12 +508,7 @@ public class AnnotationParser implements Serializable {
 		}
 	}
 
-	public static void main(String[] args) {
-		boolean b = showAnnotationsMessage();
-		System.out.println(b);
-	}
-
-	public static File selectUserDefinedAnnotation(DSDataSet dataset) {
+	public static File selectUserDefinedAnnotation(DSDataSet<? extends DSBioObject> dataset) {
 		PropertiesManager properties = PropertiesManager.getInstance();
 		String annotationDir = ".";
 		try {
@@ -655,158 +534,9 @@ public class AnnotationParser implements Serializable {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			String chipType = "User Defined";
 			return userAnnotations;
 		} else {
 			return null;
 		}
-	}
-
-	// Custom annotations loaded by the user (not necessarily Affy)
-	static class CustomAnnotations {
-
-		public CustomAnnotations() {
-			annotations = new ListOrderedMap<String, Map<String, String>>();
-		}
-
-		private ListOrderedMap<String, Map<String, String>> annotations;
-	}
-
-	private static ListOrderedMap<String, Map<String, String>> getCustomAnnots(
-			DSDataSet dataSet) {
-		CustomAnnotations annots = customAnnotations.get(dataSet);
-		if (annots == null) {
-			annots = new CustomAnnotations();
-			customAnnotations.put(dataSet, annots);
-		}
-		return annots.annotations;
-	}
-
-	/**
-	 * Takes a file in CSV format and parses out custom annotations. The first
-	 * row has the annotation names starting in column 2. The first column has
-	 * the dataset item names starting in row 2. For example, for a marker
-	 * annotation file: <table>
-	 * <tr>
-	 * <td>(blank)</td>
-	 * <td>Gene Name</td>
-	 * <td>Pathway</td>
-	 * </tr>
-	 * <tr>
-	 * <td>1973_s_at</td>
-	 * <td>MYC</td>
-	 * <td>Example // KEGG</td>
-	 * </tr>
-	 * <tr>
-	 * <td>1974_s_at</td>
-	 * <td>TP53</td>
-	 * <td>Example // KEGG</td>
-	 * </tr>
-	 * </table> etc.
-	 * 
-	 * @param file
-	 *            the file in CSV format.
-	 * @param dataSet
-	 *            the data set to annotate.
-	 * @return true if sucessfully parsed, false otherwise.
-	 */
-	public static boolean parseCustomAnnotations(File file, DSDataSet dataSet) {
-		try {
-			ListOrderedMap<String, Map<String, String>> customAnnots = getCustomAnnots(dataSet);
-			String[][] data = CSVParser.parse(new FileReader(file));
-			int columns = data[0].length - 1;
-			for (int i = 0; i < columns; i++) {
-				int c = i + 1;
-				String header = data[0][c];
-				Map<String, String> map = new HashMap<String, String>();
-				customAnnots.put(header, map);
-				for (int j = 1; j < data.length; j++) {
-					String existing = map.get(data[j][0]);
-					if (existing == null) {
-						map.put(data[j][0], data[j][c]);
-					} else {
-						map.put(data[j][0], existing + Gene.genesSeparator + data[j][c]);
-					}
-				}
-			}
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	/**
-	 * Gets a set of the names of all custom annotations.
-	 * 
-	 * @param dataSet
-	 *            the data set for which to find annotations.
-	 */
-	public static Set<String> getCustomAnnotations(DSDataSet dataSet) {
-		ListOrderedMap<String, Map<String, String>> customAnnots = getCustomAnnots(dataSet);
-		return customAnnots.keySet();
-	}
-
-	/**
-	 * Gets a custom annotation value.
-	 * 
-	 * @param annotation
-	 *            the annotation name
-	 * @param item
-	 *            the item name for which to find a value
-	 * @param dataSet
-	 *            the data set for which to look up the annotation
-	 */
-	public static String getCustomAnnotationValue(String annotation,
-			String item, DSDataSet dataSet) {
-		ListOrderedMap<String, Map<String, String>> customAnnots = getCustomAnnots(dataSet);
-		Map<String, String> map = customAnnots.get(annotation);
-		if (map == null) {
-			return null;
-		} else {
-			return map.get(item);
-		}
-	}
-
-	/**
-	 * Gets a grouping of the items in the dataset by annotation.
-	 * 
-	 * @param annotation
-	 *            the annotation by which to group.
-	 * @param annotationSeparator
-	 *            the separator sequence to use if the annotation can have
-	 *            compound values ('///' for Affy annotations).
-	 * @param dataSet
-	 *            the data set for which to look up annotations.
-	 * @return a map of all annotation values to the list of item names that
-	 *         have that value.
-	 */
-	public static Map<String, List<String>> getCustomAnnotationGroupings(
-			String annotation, String annotationSeparator, DSDataSet dataSet) {
-		ListOrderedMap<String, Map<String, String>> customAnnots = getCustomAnnots(dataSet);
-		if (annotationSeparator == null) {
-			annotationSeparator = MAIN_DELIMITER;
-		}
-		Map<String, List<String>> groups = new HashMap<String, List<String>>();
-		Map<String, String> map = customAnnots.get(annotation);
-		if (map == null) {
-			return null;
-		} else {
-			Set<String> keys = map.keySet();
-			for (String key : keys) {
-				String values = map.get(key);
-				String[] tokens = values.split(annotationSeparator);
-				for (int i = 0; i < tokens.length; i++) {
-					String value = tokens[i].trim();
-					List<String> group = groups.get(value);
-					if (group == null) {
-						group = new ArrayList<String>();
-						groups.put(value, group);
-					}
-					group.add(key);
-				}
-			}
-		}
-		return groups;
 	}
 }
