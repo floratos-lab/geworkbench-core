c130a2ececbc92f614c553915e9e44b6bfc12df0
Zhou Ji <zji@c2b2.columbia.edu>
2009-12-08 23:07:22 +0000

    remove two unused lists: geneNameMap and chipTypes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5802 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/APSerializable.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/markers/annotationparser/APSerializable.java
@@ -1,12 +1,8 @@
 package org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
-import org.apache.commons.collections15.map.ListOrderedMap;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 import org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser.AnnotationParser.MarkerAnnotation;
@@ -19,25 +15,17 @@ public class APSerializable implements Serializable {
 	private static final long serialVersionUID = 6455427625940524515L;
 
 	DSDataSet<? extends DSBioObject> currentDataSet = null;
-	Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes = new HashMap<DSDataSet<? extends DSBioObject>, String>();
-
-	Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap = new HashMap<String, ListOrderedMap<String, Vector<String>>>();
-	ArrayList<String> chipTypes = new ArrayList<String>();
-	
+	Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes = null;
 	Map<String, MarkerAnnotation> chipTypeToAnnotation = null;
 
 
 	public APSerializable(
 			DSDataSet<? extends DSBioObject> currentDataSet2,
 			Map<DSDataSet<? extends DSBioObject>, String> datasetToChipTypes,
-			Map<String, ListOrderedMap<String, Vector<String>>> geneNameMap,
-			ArrayList<String> chipTypes,
 			Map<String, MarkerAnnotation> chipTypeToAnnotation) {
+
 		this.currentDataSet = currentDataSet2;
 		this.datasetToChipTypes = datasetToChipTypes;
-		this.geneNameMap = geneNameMap;
-		this.chipTypes = chipTypes;
-		
 		this.chipTypeToAnnotation = chipTypeToAnnotation;
 	}
 }
