dd1dfe8485ed33e9c7ae81f319df6503e4985589
Oleg Shteynbuk <os2201@c2b2.columbia.edu>
2009-12-10 22:39:14 +0000

    added functions and const, updated comments
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5820 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/FilePathnameUtils.java
+++ src/org/geworkbench/util/FilePathnameUtils.java
@@ -1,27 +1,28 @@
 package org.geworkbench.util;
 
-import java.io.File;
-
 /**
  * Part of the API for accessing file system by geworkbench components.
  *
  * Functions below will convert relative path to absolute path that will start
  * with user home directory as a root.
  *
- * All file system related properties that will be handled here. User can change
+ * All file system related properties will be handled here. User can change
  * properties but all properties processing will be done here. If user didn't
- * provide file related properties defaults will be used instead.
- * All file related properties are read when class is loaded
- * and they can't be changed at run time.
+ * provide file related properties - defaults will be used instead.
+ *
+ * All file related properties are read when class is loaded and they can't be
+ * changed at run time. To specify geworkbench location for read access
+ * "components.dir" property should be used.
+ * NOTE: consider changing comments in application.properties file.
  *
  * System related file separator is created as a const so user don't have to use
- * system property or File.separator, this const should be rarely used, if at all -
- * as path creating activity should be refactored into this API.
+ * system property or File.separator, this const should be rarely used, if at
+ * all - as path creating activity should be refactored into this API.
  *
  * Currently this API is about String manipulations and creating absolute path.
  * When creating java.io.File user should use only File(String pathname)
  * constructor and use this API to create pathname string.
- * Note: relative path shouldn't be used as a pathname(can we enforce it?)
+ * NOTE: relative path shouldn't be used as a pathname(can we enforce it?)
  *
  * It is also recommended not to hardcode file names but use constants or
  * properties instead.
@@ -41,20 +42,76 @@ public class FilePathnameUtils {
 			+ "GEAW";
 	private static final String DEFAULT_HOUSEKEEPINGNORMALIZERSETTINGS_FILE = DEFAULT_TEMP_FILE_DIR
 			+ FILE_SEPARATOR + "housekeepingnormalizerSettings.config";
+	private static final String DEFAULT_DATA_FILES_DIR = "dataFiles";
+	private static final String DEFAULT_USERSETTINGS_FILE = DEFAULT_TEMP_FILE_DIR
+			+ FILE_SEPARATOR + "userSettings.config";
 
 	// file related properties
 	private static final String USER_SETTING_DIR = System
-					.getProperty("user.setting.directory");
-	private static final String TEMP_FILE_DIR = System.getProperty(
-					"temporary.files.directory");
-	private static final String HOUSEKEEPINGNORMALIZERSETTINGS_FILE = System.getProperty("housekeepingnormalizerSettings");
-	private static final String USER_HOME_DIR = System.getProperty("user.home");
+			.getProperty("user.setting.directory");
+	private static final String TEMP_FILE_DIR = System
+			.getProperty("temporary.files.directory");
+	private static final String HOUSEKEEPINGNORMALIZERSETTINGS_FILE = System
+			.getProperty("housekeepingnormalizerSettings");
+	private static final String DATA_FILES_DIR = System
+			.getProperty("data.files.dir");
+	private static final String USERSETTINGS_FILE = System
+			.getProperty("userSettings");
 
+	private static final String USER_HOME_DIR = System.getProperty("user.home");
 
 	// absolute path
 	private static String userSettingDirectoryPath = null;
 	private static String temporaryFilesDirectoryPath = null;
 	private static String housekeepingnormalizersettingsFilePath = null;
+	private static String dataFilesDirPath = null;
+	private static String userSettingsFilePath = null;
+
+	/**
+	 * will create absolute path starting with home directory as a root for user settings
+	 * file, if "userSettings" property is not set will use DEFAULT_USERSETTINGS_FILE
+	 *
+	 * @return user settings file as an absolute path
+	 *
+	 */
+	public static String getUserSettingsFilePath() {
+		if (userSettingsFilePath == null) {
+			String tempFolder = USERSETTINGS_FILE;
+			if (tempFolder == null) {
+				tempFolder = DEFAULT_USERSETTINGS_FILE;
+			}
+
+			// keep data files directory under user setting directory
+			userSettingsFilePath = getUserSettingDirectoryPath() + tempFolder
+					+ FILE_SEPARATOR;
+		}
+
+		return userSettingsFilePath;
+	}
+
+	/**
+	 * will create absolute path starting with home directory as a root for data
+	 * files directory if "data.files.dir" property is not set
+	 * will use DEFAULT_DATA_FILES_DIR
+	 *
+	 * @return user settings directory as an absolute path
+	 *
+	 */
+	public static String getDataFilesDirPath() {
+		if (dataFilesDirPath == null) {
+			String tempFolder = DATA_FILES_DIR;
+			if (tempFolder == null) {
+				tempFolder = DEFAULT_DATA_FILES_DIR;
+			}
+
+			// keep data files directory under user setting directory
+			dataFilesDirPath = getUserSettingDirectoryPath() + tempFolder
+					+ FILE_SEPARATOR;
+		}
+
+		return dataFilesDirPath;
+	}
+
 	/**
 	 * will create absolute path starting with home directory as a root for
 	 * temporary files directory if "temporary.files.directory" property is not
@@ -64,14 +121,15 @@ public class FilePathnameUtils {
 	 *
 	 */
 	public static String getTemporaryFilesDirectoryPath() {
-		if (temporaryFilesDirectoryPath == null){
+		if (temporaryFilesDirectoryPath == null) {
 			String tempFolder = TEMP_FILE_DIR;
 			if (tempFolder == null) {
 				tempFolder = DEFAULT_TEMP_FILE_DIR;
 			}
 
 			// keep temporary files directory under user setting directory
-			temporaryFilesDirectoryPath = getUserSettingDirectoryPath()+ tempFolder  + FILE_SEPARATOR;
+			temporaryFilesDirectoryPath = getUserSettingDirectoryPath()
+					+ tempFolder + FILE_SEPARATOR;
 		}
 
 		return temporaryFilesDirectoryPath;
@@ -86,7 +144,7 @@ public class FilePathnameUtils {
 	 *
 	 */
 	public static String getUserSettingDirectoryPath() {
-		if (userSettingDirectoryPath == null){
+		if (userSettingDirectoryPath == null) {
 			String userSettingDirectory = USER_SETTING_DIR;
 			if (userSettingDirectory == null) {
 				userSettingDirectory = DEFAULT_USER_SETTING_DIR;
@@ -101,22 +159,24 @@ public class FilePathnameUtils {
 
 	/**
 	 * will create absolute path starting with home directory as a root for
-	 * housekeepingnormalizersettings file if "housekeepingnormalizerSettings" property
-	 *  is not set - will use DEFAULT_HOUSEKEEPINGNORMALIZERSETTINGS_FILE
+	 * housekeepingnormalizersettings file if "housekeepingnormalizerSettings"
+	 * property is not set - will use
+	 * DEFAULT_HOUSEKEEPINGNORMALIZERSETTINGS_FILE
 	 *
-	 *  consider placing file under user setting directory,
-	 *  then there will be no need to add user setting directory manually.
+	 * consider placing file under user setting directory, then there will be no
+	 * need to add user setting directory manually.
 	 *
 	 * @return housekeepingnormalizersettings file as an absolute path
 	 *
 	 */
-		public static String getHousekeepingnormalizerSettingsPath(){
-		if (housekeepingnormalizersettingsFilePath == null){
+	public static String getHousekeepingnormalizerSettingsPath() {
+		if (housekeepingnormalizersettingsFilePath == null) {
 			String housekeepingnormalizersettingsFile = HOUSEKEEPINGNORMALIZERSETTINGS_FILE;
-			if (housekeepingnormalizersettingsFile == null){
+			if (housekeepingnormalizersettingsFile == null) {
 				housekeepingnormalizersettingsFile = DEFAULT_HOUSEKEEPINGNORMALIZERSETTINGS_FILE;
 			}
-			// keep housekeepingnormalizersettings file under user home directory
+			// keep housekeepingnormalizersettings file under user home
+			// directory
 			housekeepingnormalizersettingsFilePath = prependHomeDirName(housekeepingnormalizersettingsFile);
 		}
 
@@ -128,13 +188,15 @@ public class FilePathnameUtils {
 	 * will create absolute path starting with home directory as a root from
 	 * relative path doesn't change parameter, use return value.
 	 *
-	 * @param relative  path - file name or directory tree that will go
-	 * 		under user home directory
+	 * @param relative
+	 *            path - file name or directory tree that will go under user
+	 *            home directory
 	 *
 	 * @return absolute path starting with user home directory as a root
 	 */
 	private static String prependHomeDirName(String name) {
-		String prependName = USER_HOME_DIR + FILE_SEPARATOR + name + FILE_SEPARATOR;
+		String prependName = USER_HOME_DIR + FILE_SEPARATOR + name
+				+ FILE_SEPARATOR;
 
 		return prependName;
 	}
