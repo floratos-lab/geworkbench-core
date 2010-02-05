d813af13fb60d2337f1e305fda8c3d5e08efbe6b
Zhou Ji <zji@c2b2.columbia.edu>
2010-02-05 15:03:45 +0000

    remove dead code
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@5994 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/markers/goterms/GoMapping.java
+++
@@ -1,6 +0,0 @@
-package org.geworkbench.bison.datastructure.bioobjects.markers.goterms;
-
-@Deprecated
-public class GoMapping {
-	// TProfiler uses this, but TProfiler is very buggy and not worth the dependency until its development is revived. 
-}
