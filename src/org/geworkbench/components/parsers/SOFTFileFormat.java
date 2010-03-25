c9405bdb31ac833a0fe383d23cf413c34c0b8d6a
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2010-03-25 19:45:36 +0000

    Got the GEO SOFT GDS parser working bit faster.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6235 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/components/parsers/SOFTFileFormat.java
+++ src/org/geworkbench/components/parsers/SOFTFileFormat.java
@@ -46,13 +46,12 @@ public class SOFTFileFormat extends DataSetFileFormat {
 	private static final String commentSign1 = "#";
 	private static final String commentSign2 = "!";
 	private static final String commentSign3 = "^";
-	private static final String columnSeperator = "\t";
-	private static final String lineSeperator = "\n";
 	private static final String[] maExtensions = { "soft", "txt", "TXT"};
 	private static final String duplicateLabelModificator = "_2";
 
 	ExpressionResource resource = new ExpressionResource();
 	CSExprMicroarraySet maSet = new CSExprMicroarraySet();
+	List<String> arrayNames = new ArrayList<String>();
 	
 	SOFTFilter maFilter = null;
 	private int possibleMarkers = 0; 
@@ -92,122 +91,7 @@ public class SOFTFileFormat extends DataSetFileFormat {
 	 * @see org.geworkbench.components.parsers.FileFormat#checkFormat(java.io.File)
 	 */
 	public boolean checkFormat(File file) throws InterruptedIOException {
-		boolean columnsMatch = true;
-		boolean noDuplicateMarkers = true;
-		boolean noDuplicateArrays = true;
-		BufferedReader reader = null;
-		ProgressMonitorInputStream progressIn = null;
-		try {
-			FileInputStream fileIn = new FileInputStream(file);
-		    progressIn = new ProgressMonitorInputStream(
-					null, "Loading data from " + file.getName(), fileIn);
-			reader = new BufferedReader(new InputStreamReader(
-					progressIn));
-			 
-			String line = null;
-			int totalColumns = 0;
-			List<String> markers = new ArrayList<String>();
-			List<String> arrays = new ArrayList<String>();
-			int lineIndex = 0;
-			int headerLineIndex = 0; 
-			
-			while ((line = reader.readLine()) != null) { // for each line
-				
-				/*
-				 * Adding comments that start with '!' and '#' from the GEO SOFT file to the Experiment Information tab 
-				 */
-				if (line.startsWith(commentSign1) || line.startsWith(commentSign2)) {
-					//Ignoring the lines that has '!dataset_table_begin' and '!dataset_table_end'
-					if(!line.equalsIgnoreCase("!dataset_table_begin") && !line.equalsIgnoreCase("!dataset_table_end")) {
-						maSet.addDescription(line.substring(1));
-					}
-				}
-				
-				
-				if ((line.indexOf(commentSign1) < 0)
-						&& (line.indexOf(commentSign2) != 0)
-						&& (line.indexOf(commentSign3) != 0)
-						&& (line.length() > 0)) {// we'll skip comments and
-					// anything before header
-					if (headerLineIndex == 0)// no header detected yet, then
-						// this is the header.
-						headerLineIndex = lineIndex;
-					String token = null;
-					int columnIndex = 0;
-					int accessionIndex = 0;
-					StringTokenizer st = new StringTokenizer(line,
-							columnSeperator + lineSeperator);
-					while (st.hasMoreTokens()) { // for each column
-						token = st.nextToken().trim();
-						if (token.equals("")) {// header
-							accessionIndex = columnIndex;
-						} else if ((headerLineIndex > 0) && (columnIndex == 0)) {
-							/*
-							 * if this line is after header, then first column
-							 * should be our marker name
-							 */
-							if (markers.contains(token)) {// duplicate markers
-								noDuplicateMarkers = false;
-								log.error("Duplicate Markers: "+token);
-								return false;
-							} else {
-								markers.add(token);
-							}
-						} else if (headerLineIndex == lineIndex) {
-							/*
-							 * this is header line for SOFT file
-							 */
-							if (arrays.contains(token)) {// duplicate arrays
-								noDuplicateArrays = false;
-								log.error("Duplicate Arrays labels " + token
-										+ " in " + file.getName());
-								return false;
-							} else {
-								arrays.add(token);
-							}
-						}
-						columnIndex++;
-						lineIndex++;
-					}
-					/* check if column match or not */
-					if (headerLineIndex > 0) {
-						/*
-						 * if this line is real data, we assume lines after
-						 * header are real data. (we might have bug here)
-						 */
-						if (totalColumns == 0) /* not been set yet */
-							totalColumns = columnIndex - accessionIndex;
-						else if (columnIndex != totalColumns)// if not equal
-							columnsMatch = false;
-					}
-				}
-			}
-			possibleMarkers = markers.size();
-			fileIn.close();
-		
-		} catch (java.io.InterruptedIOException ie) {
-			if ( progressIn.getProgressMonitor().isCanceled())
-			{			    
-				throw ie;				 
-			}			 
-			else
-			   ie.printStackTrace();
-		} catch (Exception e) {
-			log.error("GEO SOFT check file format exception: " + e);
-			e.printStackTrace();
-			 
-		} finally {
-    		try {
-				reader.close();
-			} catch (IOException e) {
-				log.error("GEO SOFT file reader close exception: " + e);
-				e.printStackTrace();
-			}
-		}
-		if (columnsMatch && noDuplicateMarkers && noDuplicateArrays)
 			return true;
-		else
-			return false;
 	}
 
 	/*
@@ -266,175 +150,119 @@ public class SOFTFileFormat extends DataSetFileFormat {
 	public DSMicroarraySet getMArraySet(File file)
 			throws InputFileFormatException, InterruptedIOException {
 
+		BufferedReader in = null;
 		final int extSeperater = '.';
-
-		try
-		{
-		 if (!checkFormat(file)) {
-			log
-					.info("SOFTFileFormat::getMArraySet - "
-							+ "Attempting to open a file that does not comply with the "
-							+ "GEO SOFT file format.");
-			 throw new InputFileFormatException(
-					"Attempting to open a file that does not comply with the "
-							+ "SOFT file format.");
-		 }
-		} catch (InterruptedIOException ie) {
-			throw ie;
-		}
-		//CSExprMicroarraySet maSet = new CSExprMicroarraySet();
-		//Adding Data Description to the Experiment Information
 		String fileName = file.getName();
 		int dotIndex = fileName.lastIndexOf(extSeperater);
 		if (dotIndex != -1) {
 			fileName = fileName.substring(0, dotIndex);
 		}
 		maSet.setLabel(fileName);
-		BufferedReader in = null;
+		
+		List<String> markers = new ArrayList<String>();
 		try {
 			in = new BufferedReader(new FileReader(file));
-			if (in != null) {
-				String header = in.readLine();
-				if (header == null) {
-					throw new InputFileFormatException("File is empty.");
-				}
-				while (header != null
-						&& (header.startsWith(commentSign1) || header
-								.startsWith(commentSign2) || header
-								.startsWith(commentSign3))
-						|| StringUtils.isEmpty(header)) {
-					header = in.readLine();
-				}
-				
-				if (header == null) {
-					throw new InputFileFormatException(
-							"File is empty or consists of only comments.\n"
-									+ "SOFT File Format expected");
-				}
-
-				
-				header = StringUtils.replace(header, "\"", "");
-
-				StringTokenizer headerTokenizer = new StringTokenizer(header,
-						columnSeperator, false);
-				int n = headerTokenizer.countTokens();
-				if (n <= 1) {
-					throw new InputFileFormatException(
-							"Attempting to open a file that does not comply with the SOFT File format.\n"
-									+ "Invalid header: " + header);
-				}
-				n -= 1;
-
-				String line = in.readLine();
-				line = StringUtils.replace(line, "\"", "");
-				int m = 0;
-
-				/* Skip first token */
-				headerTokenizer.nextToken();
-				int duplicateLabels = 0;
-
-				for (int i = 0; i < n; i++) {
-					String arrayName = headerTokenizer.nextToken();
-					CSMicroarray array = new CSMicroarray(i, possibleMarkers,
-							arrayName, null, null, false,
-							DSMicroarraySet.affyTxtType);
-					maSet.add(array);
-					
-					if (maSet.size() != (i + 1)) {
-						log.info("We got a duplicate label of array");
-						array.setLabel(array.getLabel()
-								+ duplicateLabelModificator);
-						maSet.add(array);
-						duplicateLabels++;
-					}
-				}
-				while ((line != null) 
-						&& (!StringUtils.isEmpty(line))
-						&& (!line.trim().startsWith(commentSign2))) {
-					String[] tokens = line.split(columnSeperator);
-					int length = tokens.length;
-					if (length != (n + 1)) {
-						log.error("Warning: Could not parse line #" + (m + 1)
-								+ ". Line should have " + (n + 1)
-								+ " lines, has " + length + ".");
-						if ((m == 0) && (length == n + 2))
-							
-							throw new InputFileFormatException(
-									"Attempting to open a file that does not comply with the "
-											+ "SOFT file format."
-											+ "\n"
-											+ "Warning: Could not parse line #"
-											+ (m + 1)
-											+ ". Line should have "
-											+ (n + 1)
-											+ " columns, but it has "
-											+ length
-											+ ".\n"
-											+ "This file looks like R's SOFT format, which needs manually add a tab in the beginning of the header to make it a valid SOFT format.");
-						else
-							throw new InputFileFormatException(
-									"Attempting to open a file that does not comply with the "
-											+ "SOFT format." + "\n"
-											+ "Warning: Could not parse line #"
-											+ (m + 1) + ". Line should have "
-											+ (n + 1) + " columns, but it has "
-											+ length + ".");
-					}
-					String markerName = new String(tokens[0].trim());
-					CSExpressionMarker marker = new CSExpressionMarker(m);
-					marker.setLabel(markerName);
-					maSet.getMarkerVector().add(m, marker);		
-					 
-					for (int i = 1; i < n; i++) {
-							String valString = "";
-							if ((i + 1) < tokens.length) {
-								valString = tokens[i + 1];
+			if(in != null){
+				try {
+					int m = 0;
+					String header = in.readLine();
+					while (header != null) {
+						/*
+					 	* Adding comments to Experiment Information tab.
+					 	*We will ignore the line which start with '!platform_table_end', '!platform_table_begin', '!sample_table_begin'
+					 	*and '!sample_table_end'
+					 	*/
+						if (header.startsWith(commentSign1) || header.startsWith(commentSign2)) {
+							if(!header.equalsIgnoreCase("!dataset_table_begin") && !header.equalsIgnoreCase("!dataset_table_end")) {
+								maSet.addDescription(header.substring(1));
 							}
-							if (valString.trim().length() == 0) {
-								// put values directly into CSMicroarray inside of
-								// maSet
-								Float v = Float.NaN;
-								CSExpressionMarkerValue markerValue = new CSExpressionMarkerValue(v);
-								maSet.get(i).setMarkerValue(m, markerValue);
-								if (v.isNaN()) {
-									markerValue.setMissing(true);
-								} else {
-									markerValue.setPresent();
+						}	
+						String[] tokens = null;
+						if(!header.startsWith(commentSign1) && !header.startsWith(commentSign2) && !header.startsWith(commentSign3)){
+							if(header.subSequence(0, 6).equals("ID_REF")){
+								tokens = header.split("\t");
+								for(int n = 2; n<tokens.length; n++){
+									arrayNames.add(tokens[n]);
 								}
-							} else {
-								float value = Float.NaN;
-								try {
-									value = Float.parseFloat(valString);
-								} catch (NumberFormatException nfe) {
-									log.error("We expect a number, but we got: "
-											+ valString);
-								}
-								// put values directly into CSMicroarray inside of
-								// maSet
-								Float v = value;
-								CSExpressionMarkerValue markerValue = new CSExpressionMarkerValue(
-										v);
-								try {
-									maSet.get(i).setMarkerValue(m, markerValue);
-								} catch (IndexOutOfBoundsException ioobe) {
-									log.error("i=" + i + ", m=" + m);
-								}
-								if (v.isNaN()) {
-									markerValue.setMissing(true);
-								} else {
-									markerValue.setPresent();
-								}
+							}
+							if(!header.subSequence(0, 6).equals("ID_REF")){
+								String[] mark = header.split("\t");
+								markers.add(mark[0]);
+								String markerName = new String(mark[0].trim());
+								CSExpressionMarker marker = new CSExpressionMarker(m);
+								marker.setLabel(markerName);
+								maSet.getMarkerVector().add(m, marker);
+								m++;
 							}
 						}
-
-					m++;
-					line = in.readLine();
-					line = StringUtils.replace(line, "\"", "");
+						header = in.readLine();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-				
-				// Set chip-type
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		//Getting the markers size to include in a loop
+		possibleMarkers = markers.size();
+		for (int i = 0; i < arrayNames.size(); i++) {
+			String arrayName = arrayNames.get(i);
+			CSMicroarray array = new CSMicroarray(i, possibleMarkers,
+					arrayName, null, null, false,
+					DSMicroarraySet.affyTxtType);
+			maSet.add(array);	
+		}
+		
+		//This buffered reader is used to put insert marker values for one sample at a time from the Series file
+		BufferedReader out = null;
+		int j = 0;
+		try {
+			out = new BufferedReader(new FileReader(file));
+			try {
+				String line = out.readLine();
+				while (line != null) {	
+					if(!line.startsWith(commentSign1) && !line.startsWith(commentSign2) && !line.startsWith(commentSign3)){
+						if(!line.subSequence(0, 6).equals("ID_REF")){
+							String[] values = line.split("\t");
+							String valString = null; 
+							for(int k = 0; k < arrayNames.size(); k++){
+								valString = values[k+2].trim();
+								if(valString == null){
+									Float v = Float.NaN;
+									CSExpressionMarkerValue markerValue = new CSExpressionMarkerValue(v);
+									maSet.get(k).setMarkerValue(j, markerValue);
+									if (v.isNaN()) {
+										markerValue.setMissing(true);
+									} else {
+										markerValue.setPresent();
+									}
+								}else { 
+									float value = Float.NaN;
+									try {
+										value = Float.parseFloat(valString);
+									} catch (NumberFormatException nfe) {
+									}
+									// put values directly into CSMicroarray inside of
+									// maSet
+									Float v = value;
+									CSExpressionMarkerValue markerValue = new CSExpressionMarkerValue(
+											v);
+									maSet.get(k).setMarkerValue(j, markerValue);
+									if (v.isNaN()) {
+										markerValue.setMissing(true);
+									} else {
+										markerValue.setPresent();
+									}
+								}		
+							}
+							j++;
+						}
+					}
+					line = out.readLine();
+				}
 				String result = null;
-				for (int i = 0; i < m; i++) {
+				for (int i = 0; i < possibleMarkers; i++) {
 					result = AnnotationParser.matchChipType(maSet, maSet
 							.getMarkerVector().get(i).getLabel(), false);
 					if (result != null) {
@@ -469,24 +297,13 @@ public class SOFTFileFormat extends DataSetFileFormat {
 					}
 
 					marker.getUnigene().set(token);
-
 				}
-			}
-		} catch (InputFileFormatException e) {
-			throw e;
-		} catch (InterruptedIOException ie) {
-			throw ie;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-    		try {
-				in.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-				
 			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		maSet.remove(0); // removing the first Column from the array since it is the 'IDENTIFIER' column data of GEO SOFT GDS file
 		labelDisp(file, maSet);
 		return maSet;
 	}
@@ -494,8 +311,7 @@ public class SOFTFileFormat extends DataSetFileFormat {
 	 * Method adds data to Array/Phenotype Sets drop down in the selection panel 
 	 */
 	public void labelDisp(File dataFile, CSExprMicroarraySet mArraySet)
-	{
-		String[] samples = tokens(dataFile); 
+	{ 
 		BufferedReader read = null;
 		String line1 = null;
 		try {
@@ -520,8 +336,8 @@ public class SOFTFileFormat extends DataSetFileFormat {
 						DSAnnotationContext<DSMicroarray> context = manager.getContext(mArraySet, lineDt);
 	                    CSAnnotationContext.initializePhenotypeContext(context);
 	                    for(int m=0; m<token.length; m++){
-	                    	for(int n=2; n<samples.length; n++){
-	                    		if(token[m].contentEquals(samples[n])){
+	                    	for(int n=2; n<arrayNames.size(); n++){
+	                    		if(token[m].contentEquals(arrayNames.get(n))){
 	                    			context.labelItem(mArraySet.get(n-2), phLabel);
 	                    		}
 	                    	}
@@ -534,7 +350,7 @@ public class SOFTFileFormat extends DataSetFileFormat {
 			}
 			
 		} catch (FileNotFoundException e) {
-		} catch (IndexOutOfBoundsException e) {	
+		} catch (IndexOutOfBoundsException e) {
 		} finally {
 			try {
 				read.close();
@@ -543,31 +359,6 @@ public class SOFTFileFormat extends DataSetFileFormat {
 			}
 		}
 			
-	}
-
-	/*
-	 * This method returns array of 'Array' names
-	 */
-	public String[] tokens(File dFile){
-		BufferedReader read1 = null;
-		String line2 = null;
-		String[] samps = null;
-		try {
-			read1 = new BufferedReader(new FileReader(dFile));
-			line2 = read1.readLine();
-			while(line2 != null){
-				String[] samp = null;
-				samp = line2.split("\t");
-				if(samp[0].contentEquals("ID_REF")){
-					samps = samp;
-				}
-				line2 = read1.readLine();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return samps;
 	}
 	/**
 	 * 
