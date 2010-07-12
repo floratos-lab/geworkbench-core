432bb3f1a8f930595541bc093519e01ee9af8826
Zhou Ji <zji@c2b2.columbia.edu>
2010-07-12 14:57:46 +0000

    continue fixing bug 2294 (a logic flaw that causes problem sometimes)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6851 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/engine/skin/Skin.java
+++ src/org/geworkbench/engine/skin/Skin.java
@@ -796,20 +796,21 @@ public class Skin extends GUIFramework {
 		HashMap<Class, List<Class>> acceptorsNew = ComponentRegistry
 				.getRegistry().getAcceptorsHashMap();
 		List<Class> componentList = acceptorsNew.get(null);
+		List<Class> newComponentList = new ArrayList<Class>();
 
 		for (Class<?> componentClass : componentList) {
 			String componentClassName = componentClass.getName();
 
 			if ( componentClassName
 					.equals("org.geworkbench.engine.WelcomeScreen") ) {
-				componentList.remove(componentClass);
 				acceptors.remove(componentClass);
 				break;
+			} else {
+				newComponentList.add(componentClass);
 			}
 		}
 
-		acceptorsNew.put(null, componentList);
-		ComponentRegistry.getRegistry().setAcceptorsHashMap(acceptorsNew);
+		acceptorsNew.put(null, newComponentList);
 
         addAppropriateComponents(acceptors, GUIFramework.VISUAL_AREA, visualDockables);
         contentPane.revalidate();
