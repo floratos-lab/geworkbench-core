46aca10f3f7211ff71ae10f840470bf21ccbfdc6
Zhou Ji <zji@c2b2.columbia.edu>
2010-07-29 16:38:39 +0000

    bug 2190: use non-recursive implementation for serializing clusters
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@6928 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/model/clusters/CSHierClusterDataSet.java
+++ src/org/geworkbench/bison/model/clusters/CSHierClusterDataSet.java
@@ -1,14 +1,22 @@
 package org.geworkbench.bison.model.clusters;
 
-import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
-import org.geworkbench.bison.datastructure.biocollections.views.DSDataSetView;
-
 import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.biocollections.views.DSDataSetView;
 
 /**
  * @author John Watkinson
+ * @version $Id$
  */
 public class CSHierClusterDataSet extends CSAncillaryDataSet implements DSHierClusterDataSet {
+	private static Log log = LogFactory.getLog(CSHierClusterDataSet.class);
 
     private HierCluster[] clusters;
     private DSDataSetView parentSet;
@@ -63,4 +71,44 @@ public class CSHierClusterDataSet extends CSAncillaryDataSet implements DSHierCl
     public void setDataSetFile(File file) {
         // no-op
     }
+    
+	/**
+	 * Customized serialization to avoid recursive call that may cause stack
+	 * overflow.
+	 * 
+	 * @param out
+	 * @throws IOException
+	 */
+	private void writeObject(ObjectOutputStream out) throws IOException {
+
+		out.writeObject(getParentDataSet());
+		out.writeObject(parentSet);
+
+		NonRecursiveClusterSet markerHierCluster = new NonRecursiveClusterSet(
+				clusters[0], MarkerHierCluster.class);
+		NonRecursiveClusterSet arrayHierCluster = new NonRecursiveClusterSet(
+				clusters[1], MicroarrayHierCluster.class);
+
+		out.writeObject(markerHierCluster);
+		out.writeObject(arrayHierCluster);
+		log.debug("finish serialization");
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void readObject(ObjectInputStream in) throws IOException,
+			ClassNotFoundException {
+
+		setLabel("Hierarchical Clustering");
+		setParent((DSDataSet) in.readObject());
+		parentSet = (DSDataSetView) in.readObject();
+
+		clusters = new HierCluster[2];
+
+		clusters[0] = ((NonRecursiveClusterSet) in.readObject())
+				.convertToHierCluster(MarkerHierCluster.class);
+		clusters[1] = ((NonRecursiveClusterSet) in.readObject())
+				.convertToHierCluster(MicroarrayHierCluster.class);
+		log.debug("finish deserialization");
+	}
+
 }
