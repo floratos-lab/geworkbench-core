649c8ed5bd76459f69718a598cb9ef89c0d298e7
Zhou Ji <zji@c2b2.columbia.edu>
2010-09-20 14:49:46 +0000

    minor cleaning-up to facilitate fixing bug 2363
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7060 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/complex/pattern/SoapParmsDataSet.java
+++ src/org/geworkbench/bison/datastructure/complex/pattern/SoapParmsDataSet.java
@@ -1,14 +1,15 @@
 package org.geworkbench.bison.datastructure.complex.pattern;
 
-import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
-import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
-import org.geworkbench.bison.util.RandomNumberGenerator;
-import org.geworkbench.util.FilePathnameUtils;
-import org.geworkbench.util.patterns.PatternDB;
-//import polgara.soapPD_wsdl.Parameters;
-
 import java.io.File;
 import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
+import org.geworkbench.bison.util.RandomNumberGenerator;
+import org.geworkbench.util.FilePathnameUtils;
 
 /**
  * <p>Title: Sequence and Pattern Plugin</p>
@@ -17,37 +18,26 @@ import java.io.IOException;
  * <p>Company: </p>
  *
  * @author not attributable
- * @version 1.0
+ * @version $Id$
  */
-
-public class SoapParmsDataSet extends CSAncillaryDataSet {
-
-    private PatternDB patternDB;
+// this class name is very misleading. it should be something like pattern discovery result
+public class SoapParmsDataSet extends CSAncillaryDataSet<DSSequence> {
+	private static final long serialVersionUID = 6919356172704233963L;
+	static Log log = LogFactory.getLog(	SoapParmsDataSet.class);
+	
     private Parameters parms = null;
-    boolean dirty = true;
-    File dataSetFile = null;
-    String label = "Undefined";
-    File resultFile;
-    /**
-     * Link Parameters to real result.
-     * @return
-     */
-    public PatternDB getPatternDB() {
-        return patternDB;
-    }
-
-    public void setPatternDB(PatternDB patternDB) {
-        this.patternDB = patternDB;
-    }
+    private File dataSetFile = null;
+    private File resultFile;
 
     /**
-     * Initate the dataset and assign a result file for that dataset.
+     * Initiate the dataset and assign a result file for that dataset.
      * @param p
      * @param name
      * @param parent
      */
-    public SoapParmsDataSet(Parameters p, String name, DSDataSet parent) {
-        super(parent, name);
+    @SuppressWarnings("unchecked")
+	public SoapParmsDataSet(Parameters p, String name, DSDataSet<? extends DSSequence> parent) {
+        super((DSDataSet<DSSequence>)parent, name);
         parms = p;
         String idString =  RandomNumberGenerator.getID();
         setID(idString);
@@ -69,48 +59,23 @@ public class SoapParmsDataSet extends CSAncillaryDataSet {
 
     }
 
+    @Override
     public File getDataSetFile() {
         return dataSetFile;
     }
 
+    @Override
     public void setDataSetFile(File _file) {
         dataSetFile = _file;
-    }
-
-    public String getName() {
-        return "Parms S:" + parms.getMinSupport() + ", T:" + parms.getMinTokens() + ", W[" + parms.getMinWTokens() + "," + parms.getWindow() + "]";
     }
 
     public Parameters getParameters() {
         return parms;
     }
 
+    @Override
     public String getDataSetName() {
-        return getName();
-    }
-
-    public boolean equals(Object ads) {
-//        if (ads instanceof SoapParmsDataSet) {
-//            SoapParmsDataSet pds = (SoapParmsDataSet) ads;
-//            if (parms.getComputePValue() != pds.parms.getComputePValue()) return false;
-//            if (parms.getCountSeq() != pds.parms.getCountSeq()) return false;
-//            if (parms.getExact() != pds.parms.getExact()) return false;
-//            if (parms.getExactTokens() != pds.parms.getExactTokens()) return false;
-//            if (parms.getGroupingN() != pds.parms.getGroupingN()) return false;
-//            if (parms.getGroupingType() != pds.parms.getGroupingType()) return false;
-//            if (parms.getMaxPatternNo() != pds.parms.getMaxPatternNo()) return false;
-//            if (parms.getMinPatternNo() != pds.parms.getMinPatternNo()) return false;
-//            if (parms.getMinPer100Support() != pds.parms.getMinPer100Support()) return false;
-//            if (parms.getMinPValue() != pds.parms.getMinPValue()) return false;
-//            if (parms.getMinSupport() != pds.parms.getMinSupport()) return false;
-//            if (parms.getMinTokens() != pds.parms.getMinTokens()) return false;
-//            if (parms.getMinWTokens() != pds.parms.getMinWTokens()) return false;
-//            if (parms.getSimilarityMatrix() != pds.parms.getSimilarityMatrix()) return false;
-//            if (parms.getSimilarityThreshold() != pds.parms.getSimilarityThreshold()) return false;
-//            if (parms.getWindow() != pds.parms.getWindow()) return false;
-//            return true;
-//        }
-        return false;
+        return "Parms S:" + parms.getMinSupport() + ", T:" + parms.getMinTokens() + ", W[" + parms.getMinWTokens() + "," + parms.getWindow() + "]";
     }
 
     /**
@@ -118,14 +83,14 @@ public class SoapParmsDataSet extends CSAncillaryDataSet {
      *
      * @param fileName String
      */
+    @Override
     public void writeToFile(String fileName) {
+    	// TODO not implemented yet
+    	log.warn("writeToFile not implemented for SoapParmsDataSet");
     }
 
     public File getResultFile() {
         return resultFile;
     }
 
-    public void setResultFile(File resultFile) {
-        this.resultFile = resultFile;
-    }
 }
