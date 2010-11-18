c396693b3be0e1b3254aa042802eecfb7424b585
Zhou Ji <zji@c2b2.columbia.edu>
2010-11-18 21:43:20 +0000

    refactor method isLogNormalized into a separate interface because there are only two particular parameter panels that have it. change logged in mantis #2400
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7203 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/bison/model/analysis/ParameterPanelIncludingNormalized.java
@@ -0,0 +1,8 @@
+package org.geworkbench.bison.model.analysis;
+
+public interface ParameterPanelIncludingNormalized {
+	/*
+     * This is needed by T-test and multi T-test.
+     */
+    public boolean isLogNormalized();
+}
