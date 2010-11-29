0ef59a8c6e300db7d4faf5d6a454c1ef4cafa507
Zhou Ji <zji@c2b2.columbia.edu>
2010-11-29 23:02:08 +0000

    cleaning up CaArrayQueryResultEvent. see mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7254 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/events/CaArrayQueryResultEvent.java
+++ src/org/geworkbench/events/CaArrayQueryResultEvent.java
@@ -1,41 +1,24 @@
 package org.geworkbench.events;
 
-import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeMap;
 
 import org.geworkbench.engine.config.events.Event;
-import org.geworkbench.engine.config.events.EventSource;
+
+/**
+ * 
+ * @author zji
+ * @version $Id$
+ */
 
 public class CaArrayQueryResultEvent extends Event {
-	private boolean populated = false;
 	private boolean succeed = true;
-	private String infoType;
 	private TreeMap<String, Set<String>> queryPairs;
-	private String url;
-	private int port;
-	private String username;
-	private String password;
-	private static String GOTVALIDVALUES = "getvalidvalues";
-	private static String GOTEXPERIMENTS = "getexperiments";
+
 	private String errorMessage;
 
-	public CaArrayQueryResultEvent(EventSource s, String url, int port,
-			String username, String password) {
-		super(s);
-
-		this.url = url;
-		this.port = port;
-		this.username = username;
-		this.password = password;
-	}
-
-	public boolean isPopulated() {
-		return populated;
-	}
-
-	public void setPopulated(boolean populated) {
-		this.populated = populated;
+	public CaArrayQueryResultEvent() {
+		super(null);
 	}
 
 	public boolean isSucceed() {
@@ -46,52 +29,12 @@ public class CaArrayQueryResultEvent extends Event {
 		this.succeed = succeed;
 	}
 
-	public String getInfoType() {
-		return infoType;
-	}
-
-	public void setInfoType(String infoType) {
-		this.infoType = infoType;
-	}
-
 	public TreeMap<String, Set<String>> getQueryPairs() {
 		return queryPairs;
 	}
 
 	public void setQueryPairs(TreeMap<String, Set<String>> queryPairs) {
 		this.queryPairs = queryPairs;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getErrorMessage() {
