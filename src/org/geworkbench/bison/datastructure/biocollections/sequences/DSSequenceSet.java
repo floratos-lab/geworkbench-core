49ce07dfe3c1d9c69e33211eba446aa000e93b61
Zhou Ji <zji@c2b2.columbia.edu>
2010-11-29 04:50:09 +0000

    remove interface DSCollection: mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7238 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/sequences/DSSequenceSet.java
+++ src/org/geworkbench/bison/datastructure/biocollections/sequences/DSSequenceSet.java
@@ -1,18 +1,19 @@
 package org.geworkbench.bison.datastructure.biocollections.sequences;
 
-import org.geworkbench.bison.datastructure.biocollections.DSCollection;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.sequence.DSSequence;
 import org.geworkbench.bison.datastructure.complex.panels.DSItemList;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.RandomAccess;
 
 /**
  * @author John Watkinson
+ * @version $Id$
  */
-public interface DSSequenceSet<T extends DSSequence> extends RandomAccess, Cloneable, Serializable, DSDataSet<T>, DSCollection<T> {
+public interface DSSequenceSet<T extends DSSequence> extends RandomAccess, Cloneable, Serializable, DSDataSet<T>, List<T> {
 
     void addASequence(T sequence);
 
