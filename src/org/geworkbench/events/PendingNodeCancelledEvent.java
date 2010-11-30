5a4051b3236593e2bde92f800260270703af8c22
Zhou Ji <zji@c2b2.columbia.edu>
2010-11-30 20:08:04 +0000

    minor cleaning. see mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7263 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/events/PendingNodeCancelledEvent.java
+++ src/org/geworkbench/events/PendingNodeCancelledEvent.java
@@ -1,21 +1,14 @@
 package org.geworkbench.events;
 
-import java.util.Collection;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.geworkbench.engine.config.events.Event;
-import org.geworkbench.engine.config.events.EventSource;
 import org.ginkgo.labs.ws.GridEndpointReferenceType;
 
 /**
  * 
  * @author kk2457
- * @version $Id: PendingNodeCancelledEvent.java,v 1.1 2008-06-11 21:12:00 chiangy Exp $
+ * @version $Id$
  */
 public class PendingNodeCancelledEvent extends Event {
-
-	private Log log = LogFactory.getLog(this.getClass());
 
 	private GridEndpointReferenceType gridEpr = null;
 
