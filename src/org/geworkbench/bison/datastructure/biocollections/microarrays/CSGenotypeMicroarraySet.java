e109cc207283288fe845981f8c0d0c79e5c0717f
Zhou Ji <zji@c2b2.columbia.edu>
2010-12-08 16:42:23 +0000

    remove dead code. see mantis #1620
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7309 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/microarrays/CSGenotypeMicroarraySet.java
+++
@@ -1,6 +0,0 @@
-package org.geworkbench.bison.datastructure.biocollections.microarrays;
-
-@Deprecated
-// dropped component (Genotypic File Format). see release 1.8 document
-public class CSGenotypeMicroarraySet {
-}
