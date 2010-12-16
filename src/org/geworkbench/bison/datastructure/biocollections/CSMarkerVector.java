92458854e5c35eac7cce9154f7c9bc8c9e1a92d3
Zhou Ji <zji@c2b2.columbia.edu>
2010-12-16 21:08:32 +0000

    cleaning-up. see mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7342 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/CSMarkerVector.java
+++ src/org/geworkbench/bison/datastructure/biocollections/CSMarkerVector.java
@@ -1,270 +1,238 @@
 package org.geworkbench.bison.datastructure.biocollections;
 
-import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
-import org.geworkbench.bison.datastructure.complex.panels.CSSequentialItemList;
-import org.geworkbench.bison.datastructure.complex.panels.DSItemList;
-import org.geworkbench.bison.util.HashVector;
-
-import java.util.List;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.complex.panels.CSSequentialItemList;
+
 /**
- * <p>Title: caWorkbench</p>
- * <p/>
- * <p>Description: Modular Application Framework for Gene Expession, Sequence
- * and Genotype Analysis</p>
- * <p/>
- * <p>Copyright: Copyright (c) 2003 -2004</p>
- * <p/>
- * <p>Company: Columbia University</p>
- *
+ * <p>
+ * Copyright: Copyright (c) 2003 -2004
+ * </p>
+ * <p>
+ * Company: Columbia University
+ * </p>
+ * 
  * @author Adam Margolin
- * @version 3.0
+ * @version $Id$
  */
-public class CSMarkerVector extends CSSequentialItemList<DSGeneMarker> implements
-        DSItemList<DSGeneMarker> {
+public class CSMarkerVector extends CSSequentialItemList<DSGeneMarker> {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = -7674561735987333555L;
-	
-	// watkin -- changed these hashvectors to not enforce uniqueness. They were slow to the point of unusability.
-    // Uniqueness is not a requirement of these data structures anyways.
-    HashVector<Integer,
-            DSGeneMarker> geneIdMap = new HashVector<Integer, DSGeneMarker>(false);
-    HashVector<String,
-            DSGeneMarker> geneNameMap = new HashVector<String, DSGeneMarker>(false);
-    boolean mapGeneNames = true;
 
-    public CSMarkerVector() {
-    }
+	private Hashtable<Integer, Set<DSGeneMarker>> geneIdMap = new Hashtable<Integer, Set<DSGeneMarker>>();
+	private Hashtable<String, Set<DSGeneMarker>> geneNameMap = new Hashtable<String, Set<DSGeneMarker>>();
 
-    public CSMarkerVector(List<DSGeneMarker> markerList) {
-        super();
-        this.addAll(markerList);
-    }
+	public DSGeneMarker getMarkerByUniqueIdentifier(String label) {
+		return super.get(label);
+	}
 
-    public DSGeneMarker getMarkerByUniqueIdentifier(String label) {
-        return super.get(label);
-    }
+	// utility to add a new item to a map's value set
+	private static <T> void addItem(Map<T, Set<DSGeneMarker>> map, T key,
+			DSGeneMarker marker) {
+		Set<DSGeneMarker> set = map.get(key);
+		if (set == null) {
+			set = new HashSet<DSGeneMarker>();
+			set.add(marker);
+			map.put(key, set);
+		} else {
+			set.add(marker);
+		}
+	}
 
-    /**
-     *
-     */
-    //There is a bug related to CSMarkerVector,  similar to the bug listed above.
-    //geneNameMap and geneIDMap in CSMarkerVector always are empty (size =1 or 0)
-    //add a new correctMaps() method in CSMarkerVector to temp fix the probelm.
-    //geneIdMap still is empty.
+	/* reset geneIdMap and genNameMap as a temporary fix */
+	/* TODO to find out whether and why this is necessary */
+	/* this method is only used in HouseKeepingGeneNormalizer */
+	public void correctMaps() {
+		geneIdMap.clear();
+		geneNameMap.clear();
+		for (DSGeneMarker item : this) {
+			Integer geneId = new Integer(item.getGeneId());
+			if (geneId != null && geneId.intValue() > 0) {
+				addItem(geneIdMap, geneId, item);
+			}
 
-    public void correctMaps() {
-        geneIdMap.clear();
-        geneNameMap.clear();
-        for (DSGeneMarker item : this) {
-            Integer geneId = new Integer(item.getGeneId());
-            if (geneId != null && geneId.intValue() != -1) {
-                geneIdMap.addItem(geneId, item);
-            }
+			String geneName = item.getGeneName();
+			String label = item.getLabel();
+			if (geneName != null && (!"---".equals(geneName))) {
+				if (label != null && geneName.equals("")) {
+					addItem(geneNameMap, label, item);
+				} else {
+					addItem(geneNameMap, geneName, item);
+				}
+			}
 
-            if (mapGeneNames) {
-                String geneName = item.getGeneName();
-                String label = item.getLabel();
-                if (geneName != null && (!"---".equals(geneName))) {
-                    if (label != null && geneName.equals("")) {
-                        geneNameMap.addItem(label, item);
-                    } else {
-                        geneNameMap.addItem(geneName, item);
-                    }
-                }
-            }
+		}
+	}
 
-        }
-    }
+	/* return all matching markers */
+	/* only used in this class and HouseKeepingGeneNormalizer */
+	public Vector<DSGeneMarker> getMatchingMarkers(String aString) {
+		Vector<DSGeneMarker> matchingMarkers = new Vector<DSGeneMarker>();
+		DSGeneMarker uniqueKeyMarker = super.get(aString);
+		if (uniqueKeyMarker != null) {
+			matchingMarkers.add(uniqueKeyMarker);
+		}
 
-    public Vector<DSGeneMarker> getMatchingMarkers(String aString) {
-        Vector<DSGeneMarker> matchingMarkers = new Vector<DSGeneMarker>();
-        DSGeneMarker uniqueKeyMarker = super.get(aString);
-        if (uniqueKeyMarker != null) {
-            matchingMarkers.add(uniqueKeyMarker);
-        }
-        try {
-            Vector<DSGeneMarker> markersSet;
-            if (mapGeneNames) {
-                markersSet = geneNameMap.get(aString);
-                int size = geneNameMap.size();
-                if (markersSet != null && markersSet.size() > 0) {
-                    for (DSGeneMarker marker : markersSet) {
-                        if (!matchingMarkers.contains(marker)) {
-                            matchingMarkers.add(marker);
-                        }
-                    }
-                }
+		Set<DSGeneMarker> markersSet = geneNameMap.get(aString);
+		if (markersSet != null && markersSet.size() > 0) {
+			for (DSGeneMarker marker : markersSet) {
+				if (!matchingMarkers.contains(marker)) {
+					matchingMarkers.add(marker);
+				}
+			}
+		}
 
-                Integer geneId = Integer.parseInt(aString);
-                markersSet = geneIdMap.get(geneId);
-                if (markersSet != null && markersSet.size() > 0) {
-                    for (DSGeneMarker marker : markersSet) {
-                        if (!matchingMarkers.contains(marker)) {
-                            matchingMarkers.add(marker);
-                        }
-                    }
-                }
+		Integer geneId = Integer.parseInt(aString);
+		markersSet = geneIdMap.get(geneId);
+		if (markersSet != null && markersSet.size() > 0) {
+			for (DSGeneMarker marker : markersSet) {
+				if (!matchingMarkers.contains(marker)) {
+					matchingMarkers.add(marker);
+				}
+			}
+		}
 
-            }
-        } catch (Exception e) {
+		return matchingMarkers;
+	}
 
-        }
-        return matchingMarkers;
-    }
+	/* return an first matching item based on a given string */
+	public DSGeneMarker get(String aString) {
+		if (aString == null) {
+			return null;
+		}
 
-    public Vector<DSGeneMarker> getMatchingMarkers(int geneId) {
-        return getMatchingMarkers(new Integer(geneId));
-    }
+		DSGeneMarker marker = super.get(aString);
+		if (marker == null) {
+			Vector<DSGeneMarker> matchingMarkers = getMatchingMarkers(aString);
+			if (matchingMarkers != null && matchingMarkers.size() > 0) {
+				marker = matchingMarkers.get(0);
+			}
+		}
+		return marker;
+	}
 
+	/* add an item to this object if it is not already in */
+	public boolean add(DSGeneMarker item) {
 
-    public Vector<DSGeneMarker> getMatchingMarkers(Integer geneId) {
-        if (geneId.intValue() == -1) {
-            return null;
-        }
+		if (item == null)
+			return false;
+		if (this.contains(item))
+			return false;
 
-        return geneIdMap.get(geneId);
-    }
+		boolean result = super.add(item);
+		if (result) {
+			Integer geneId = new Integer(item.getGeneId());
+			if (geneId != null && geneId.intValue() > 0) {
+				addItem(geneIdMap, geneId, item);
+			}
 
-    public DSGeneMarker get(String aString) {
-        if (aString == null) {
-            return null;
-        }
+			String geneName = item.getGeneName();
+			if (geneName != null && (!"---".equals(geneName))) {
+				addItem(geneNameMap, geneName, item);
+			}
 
-        DSGeneMarker marker = super.get(aString);
-        if (marker == null) {
-            Vector<DSGeneMarker> matchingMarkers = getMatchingMarkers(aString);
-            if (matchingMarkers != null && matchingMarkers.size() > 0) {
-                marker = matchingMarkers.get(0);
-            }
-        }
-        return marker;
-    }
+		}
 
-    public boolean add(DSGeneMarker item) {
-        boolean result = false;
-        if (item != null) {
-            if (!this.contains(item)) {
-                result = super.add(item);
-                if (result) {
-                    Integer geneId = new Integer(item.getGeneId());
-                    if (geneId != null && geneId.intValue() != -1) {
-                        geneIdMap.addItem(geneId, item);
-                    }
+		return result;
+	}
 
-                    if (mapGeneNames) {
-                        String geneName = item.getGeneName();
-                        String label = item.getLabel();
-                        if (geneName != null && (!"---".equals(geneName))) {
-                            geneNameMap.addItem(geneName, item);
+	/* add an item at the given index */
+	public void add(int i, DSGeneMarker item) {
+		super.add(i, item);
+		Integer geneId = new Integer(item.getGeneId());
+		if (geneId != null && geneId.intValue() > 0) {
+			addItem(geneIdMap, geneId, item);
+		}
 
-                        }
+		String geneName = item.getGeneName();
+		if (geneName != null && (!"---".equals(geneName))) {
+			addItem(geneNameMap, geneName, item);
+		}
 
-                    }
-                }
-            }
-        }
-        return result;
-    }
+	}
 
-    public void add(int i, DSGeneMarker item) {
-        super.add(i, item);
-        Integer geneId = new Integer(item.getGeneId());
-        if (geneId != null && geneId.intValue() != -1) {
-            geneIdMap.addItem(geneId, item);
-        }
+	/* return the fist matching item */
+	public DSGeneMarker get(DSGeneMarker item) {
+		DSGeneMarker marker = super.get(item);
+		if (marker == null) {
+			Vector<DSGeneMarker> matchingMarkers = getMatchingMarkers(item);
+			if (matchingMarkers != null && matchingMarkers.size() > 0) {
+				marker = matchingMarkers.get(0);
+			}
+		}
 
-        if (mapGeneNames) {
-            String geneName = item.getGeneName();
-            if (geneName != null && (!"---".equals(geneName))) {
-                geneNameMap.addItem(geneName, item);
-            }
-        }
+		return marker;
+	}
 
-    }
+	// TODO the callers of this method, CSMicroarraySet and
+	// HouseKeepingGeneNormalizer, should use set instead of vector.
+	public Vector<DSGeneMarker> getMatchingMarkers(DSGeneMarker item) {
+		return new Vector<DSGeneMarker>(getMatchingMarkersSet(item));
+	}
 
-    public DSGeneMarker get(DSGeneMarker item) {
-        DSGeneMarker marker = super.get(item);
-        if (marker == null) {
-            Vector<DSGeneMarker> matchingMarkers = getMatchingMarkers(item);
-            if (matchingMarkers != null && matchingMarkers.size() > 0) {
-                marker = matchingMarkers.get(0);
-            }
-        }
+	// TODO to replace the Vector version later
+	private Set<DSGeneMarker> getMatchingMarkersSet(DSGeneMarker item) {
+		if ((item.getGeneName() != null) && (item.getGeneName().length() > 0)) {
+			return geneNameMap.get(item.getGeneName());
+		} else {
+			return geneIdMap.get(new Integer(item.getGeneId()));
+		}
+	}
 
-        //        if(marker == null){
-        //            System.out.println("null marker");
-        //        }
-        return marker;
-    }
+	/* check if this object contains the given item */
+	public boolean contains(Object item) {
+		if (item instanceof DSGeneMarker) {
+			// contains method in the super class CSItemList is not public, so
+			// we need to use get instead contains to do this
+			DSGeneMarker marker = super.get((DSGeneMarker) item);
+			if (marker != null) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 
-    public Vector<DSGeneMarker> getMatchingMarkers(DSGeneMarker item) {
-        if (mapGeneNames && (item.getGeneName() != null) &&
-            (item.getGeneName().length() > 0)) {
-            return geneNameMap.get(item.getGeneName());
-        } else {
-            return geneIdMap.get(new Integer(item.getGeneId()));
-        }
-    }
+	/* remove an item from this object */
+	public boolean remove(Object item) {
+		boolean removed = super.remove(item);
+		if (removed && item instanceof DSGeneMarker) {
+			geneIdMap.remove(new Integer(((DSGeneMarker) item).getGeneId()));
+		}
+		return removed;
+	}
 
-    public boolean contains(Object item) {
-        if (item instanceof DSGeneMarker) {
-            return this.contains((DSGeneMarker) item);
-        } else {
-            return false;
-        }
-    }
+	/* remove the item at the given index from this class */
+	public DSGeneMarker remove(int index) {
+		DSGeneMarker marker = super.remove(index);
+		geneIdMap.remove(new Integer(marker.getGeneId()));
+		return marker;
+	}
 
-    public boolean contains(DSGeneMarker item) {
-        // Contains must use a strict sense of equality!
-        DSGeneMarker marker = super.get(item);
-        if (marker != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/* reset this class to be clear */
+	public void clear() {
+		super.clear();
+		geneIdMap.clear();
+	}
 
-    //To change
-    public boolean remove(Object item) {
-        boolean removed = super.remove(item);
-        if (removed) {
-            if (item instanceof DSGeneMarker) {
-                geneIdMap.remove(new Integer(((DSGeneMarker)item).getGeneId()));
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/* used in AffyFileFormat and GenPixFileFormat */
+	public void setLabel(int index, String label) {
+		DSGeneMarker item = get(index);
+		if (item == null)
+			return;
 
-    public DSGeneMarker remove(int index) {
-        DSGeneMarker marker = super.remove(index);
-        geneIdMap.remove(new Integer(marker.getGeneId()));
-        return marker;
-    }
-
-    public void clear() {
-        super.clear();
-        geneIdMap.clear();
-    }
-
-    public boolean setLabel(int index, String label) {
-        DSGeneMarker item = get(index);
-        if (item != null) {
-            String oldLabel = item.getLabel();
-            if (oldLabel != null) {
-                objectMap.remove(oldLabel);
-            }
-            item.setLabel(label);
-            objectMap.put(label, item);
-            return true;
-        } else {
-            return false;
-        }
-    }
+		String oldLabel = item.getLabel();
+		if (oldLabel != null) {
+			objectMap.remove(oldLabel);
+		}
+		item.setLabel(label);
+		objectMap.put(label, item);
+	}
 }
