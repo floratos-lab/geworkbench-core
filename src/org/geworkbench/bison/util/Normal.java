bfcacbf0988a56bfb69fc8ca3dc9511932eb2761
Zhou Ji <zji@c2b2.columbia.edu>
2010-12-21 22:26:25 +0000

    cleaning-up. see mantis #2418
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7363 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/util/Normal.java
+++ src/org/geworkbench/bison/util/Normal.java
@@ -9,25 +9,19 @@ import java.io.Serializable;
  * <p>Company: Columbia University</p>
  *
  * @author Andrea Califano
- * @version 1.0
+ * @version $Id$
  */
 
 public class Normal implements Serializable {
-    double mean;
-    double sigma;
-    double variance;
-    double n;
-    double sumX;
-    double sumXX;
-    boolean status; // 0 uncomputed, 1 computed, -1 Invalid
-
-    public void invalidate() {
-        status = true;
-    }
-
-    public boolean isValid() {
-        return (status);
-    }
+	private static final long serialVersionUID = 815454764380381809L;
+	
+	private double mean;
+	private double sigma;
+	private double variance;
+	private double n;
+	private double sumX;
+	private double sumXX;
+	private boolean status;
 
     public void add(double v) {
         sumX += v;
@@ -36,32 +30,18 @@ public class Normal implements Serializable {
         status = false;
     }
 
-    public void remove(double v) {
-        sumX -= v;
-        sumXX -= v * v;
-        n--;
-        status = false;
-    }
-
-    public void compute() {
+    private void compute() {
         if (!status) {
             if (n > 1) {
                 mean = sumX / n;
                 variance = (sumXX - n * mean * mean) / (n - 1);
                 sigma = Math.sqrt(variance);
-                //        if(sigma < 0.01) {
-                //          sigma = 0.01;
-                //          variance = sigma * sigma;
-                //        }
+
                 status = true;
             } else {
                 status = true;
             }
         }
-    }
-
-    public double getN() {
-        return n;
     }
 
     public double getMean() {
@@ -74,38 +54,7 @@ public class Normal implements Serializable {
         return sigma;
     }
 
-    public double getVariance() {
-        compute();
-        return variance;
-    }
-
-    public void set(double mean, double sigma) {
-        this.mean = mean;
-        this.sigma = sigma;
-        if (this.sigma < 0.01) {
-            this.sigma = 0.01;
-        }
-        variance = sigma * sigma;
-        status = true;
-    }
-
-    public double getP(double v) {
-        final double piCoeff = Math.sqrt(Math.PI);
-        double dist = Math.abs(v - mean);
-        double t0 = Math.exp(-(dist * dist) / (2 * sigma * sigma));
-        double t1 = t0 / (sigma * piCoeff);
-        return t1;
-    }
-
-    public boolean inInterval(double x, double coeff) {
-        return (Math.abs(x - mean) < sigma * coeff);
-    }
-
     public Normal() {
-        reset();
-    }
-
-    public void reset() {
         mean = 0;
         n = 0;
         variance = 0;
@@ -115,22 +64,4 @@ public class Normal implements Serializable {
         status = false;
     }
 
-    public Normal(double mean, double sigma) {
-        reset();
-        set(mean, sigma);
-    }
-
-    void getInterval(double min, double max, double coeff) {
-        compute();
-        min = mean - sigma * coeff;
-        max = mean + sigma * coeff;
-    }
-
-    static public double tTest(Normal a, Normal b) {
-        double numerator = Math.abs(a.getMean() - b.getMean());
-        double denominator = Math.sqrt(a.getVariance() / a.getN() + b.getVariance() / b.getN());
-        if (denominator == 0)
-            denominator = 0.000000001;
-        return numerator / denominator;
-    }
 }
