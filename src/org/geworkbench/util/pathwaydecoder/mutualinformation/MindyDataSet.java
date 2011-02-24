3753818a497bd5d6a6a14f1d845b84001d012696
Zhou Ji <zji@c2b2.columbia.edu>
2011-02-24 16:58:20 +0000

    cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7491 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/util/pathwaydecoder/mutualinformation/MindyDataSet.java
+++ src/org/geworkbench/util/pathwaydecoder/mutualinformation/MindyDataSet.java
@@ -3,22 +3,22 @@ package org.geworkbench.util.pathwaydecoder.mutualinformation;
 import java.io.File;
 
 import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
-import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
-import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
 
 /**
  * @author Matt Hall
  * @author oshteynb
- * @version $Id: MindyDataSet.java,v 1.6 2009-04-29 19:59:39 oshteynb Exp $
+ * @version $Id$
  */
-public class MindyDataSet extends CSAncillaryDataSet implements DSAncillaryDataSet {
+public class MindyDataSet extends CSAncillaryDataSet<DSMicroarray> {
 
     private static final long serialVersionUID = -6835973287728524201L;
     private MindyData data;
     private String filename;
 
 
-	public MindyDataSet(DSDataSet parent, String label, MindyData data, String filename) {
+	public MindyDataSet(DSMicroarraySet<DSMicroarray> parent, String label, MindyData data, String filename) {
         super(parent, label);
         this.data = data;
         this.filename = filename;
