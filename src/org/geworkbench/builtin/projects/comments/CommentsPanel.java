73ce640817a02720969d12be2dd6155396ef14ef
Zhou Ji <zji@c2b2.columbia.edu>
2011-03-03 18:58:52 +0000

    cleaning-up
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7536 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/builtin/projects/comments/CommentsPanel.java
+++ src/org/geworkbench/builtin/projects/comments/CommentsPanel.java
@@ -19,39 +19,21 @@ import java.awt.*;
  * <p>Company: First Genetic Trust Inc.</p>
  *
  * @author First Genetic Trust
- * @version 1.0
+ * @version $Id$
  */
-@AcceptTypes({DSDataSet.class}) public class CommentsPanel implements VisualPlugin {
+@AcceptTypes({DSDataSet.class}) 
+public class CommentsPanel implements VisualPlugin {
 
     static Log log = LogFactory.getLog(CommentsPanel.class);
 
     /**
-     * Used as the "name" part in the name-value pair that stores the user
-     * comments in a <code>MicroarraySet</code> object X. In particular, if
-     * the array returned by a call to
-     * <code>X.getValuesForName(COMMENTS_ID_STRING)</code> is not null, then
-     * the first entry in the array will contain the user comments associated
-     * with the microarray set X.
-     */
-    private final String COMMENTS_ID_STRING = "User Comments";
-    /**
      * Text to display when there are no user comments entered.
      */
     private final String DEFAULT_MESSAGE = "";
-    /**
-     * The currently selected microarray set.
-     */
-    protected DSDataSet dataSet = null;
-    protected ImageIcon image = null;
-    private BorderLayout borderLayout1 = new BorderLayout();
-    protected JScrollPane jScrollPane1 = new JScrollPane();
-    protected JTextArea commentsTextArea = new JTextArea(DEFAULT_MESSAGE);
-    protected JPanel commentsPanel = new JPanel();
-    private boolean liveMode = true;
 
-    public String getName() {
-        return "Comments Pane";
-    }
+    private JTextArea commentsTextArea = new JTextArea(DEFAULT_MESSAGE);
+    private JPanel commentsPanel = new JPanel();
+    private boolean liveMode = true;
 
     public CommentsPanel() {
         try {
@@ -63,7 +45,7 @@ import java.awt.*;
     }
 
     private void jbInit() throws Exception {
-        commentsPanel.setLayout(borderLayout1);
+        commentsPanel.setLayout(new BorderLayout());
         commentsTextArea.setLineWrap(true);
         commentsTextArea.setWrapStyleWord(true);
         commentsTextArea.getDocument().addDocumentListener(new DocumentListener() {
@@ -80,45 +62,11 @@ import java.awt.*;
             }
 
         });
+        JScrollPane jScrollPane1 = new JScrollPane();
         commentsPanel.add(jScrollPane1, BorderLayout.CENTER);
         jScrollPane1.getViewport().add(commentsTextArea, null);
-//        JButton loadCustomButton = new JButton("Load Custom Data Annotations...");
-//        JPanel loadPanel = new JPanel();
-//        loadPanel.setLayout(new BoxLayout(loadPanel, BoxLayout.X_AXIS));
-//        loadPanel.add(loadCustomButton);
-//        commentsPanel.add(loadPanel, BorderLayout.SOUTH);
-//        loadCustomButton.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                loadCustomAnnotations();
-//            }
-//        });
-    }
 
-//    private void loadCustomAnnotations() {
-//        JFileChooser fc = new JFileChooser(".");
-//        javax.swing.filechooser.FileFilter filter = new FileFilter() {
-//
-//            public String getDescription() {
-//                return "Comma Separated Values Files";
-//            }
-//
-//            public boolean accept(File f) {
-//                boolean returnVal = false;
-//                if (f.isDirectory() || f.getName().toLowerCase().endsWith(".csv")) {
-//                    return true;
-//                }
-//                return returnVal;
-//            }
-//
-//        };
-//        fc.setFileFilter(filter);
-//        fc.setDialogTitle("Open Marker Set");
-//        int choice = fc.showOpenDialog(commentsPanel.getParent());
-//        if (choice == JFileChooser.APPROVE_OPTION) {
-//            // Load custom annotations in annotation parser
-//            AnnotationParser.parseCustomAnnotations(fc.getSelectedFile(), dataSet);
-//        }
-//    }
+    }
 
     public Component getComponent() {
         return commentsPanel;
@@ -129,21 +77,11 @@ import java.awt.*;
             return;
         }
         String userComments = commentsTextArea.getText().trim();
-//        // No action required if nothing changed.
-//        if (commentsTextArea.getText().trim().equals(userComments.trim())) {
-//            return;
-//        }
+
         if (liveMode) {
             publishCommentsEvent(new CommentsEvent(userComments));
         }
         
-//        if (dataSet != null) {
-//            dataSet.clearName(COMMENTS_ID_STRING);
-//            dataSet.addNameValuePair(COMMENTS_ID_STRING, userComments);
-//            publishCommentsEvent(new org.geworkbench.events.CommentsEventOld(dataSet, userComments));
-//        } else if (image != null) {
-//            image.setDescription(userComments);
-//        }
     }
 
     @Publish public CommentsEvent publishCommentsEvent(CommentsEvent event) {
@@ -156,40 +94,5 @@ import java.awt.*;
         commentsTextArea.setCaretPosition(0);
         liveMode = true;
     }
-    
   
-
-    /**
-     * Application listener for receiving events that modify the currently
-     * selected microarray set.
-     *
-     * @param e
-     */
-//    @Subscribe
-//    public void receive(ProjectEvent e, Object source) {
-//        DSDataSet dataSet = e.getDataSet();
-//        if (e.getMessage().equals(ProjectEvent.CLEARED)) {
-//            this.dataSet = null;
-//            userComments = DEFAULT_MESSAGE;
-//        } else if (dataSet != this.dataSet) {
-//            this.dataSet = dataSet;
-//            image = null;
-//            userComments = DEFAULT_MESSAGE;
-//            Object[] values = this.dataSet.getValuesForName(COMMENTS_ID_STRING);
-//            if (values != null && values.length > 0) {
-//                userComments = (String) values[0];
-//                if (userComments.trim().equals(""))
-//                    userComments = DEFAULT_MESSAGE;
-//            }
-//        }
-//        commentsTextArea.setText(userComments);
-//        commentsTextArea.setCaretPosition(0);   // For long text.
-//    }
-//
-//    @Subscribe
-//    public void receive(ImageSnapshotEvent event, Object source) {
-//        image = event.getImage();
-//        dataSet = null;
-//        commentsTextArea.setText(image.getDescription());
-//    }
 }
