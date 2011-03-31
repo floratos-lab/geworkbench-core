e3b1edf4b2395f57ffd66897788792e643519b37
Zheng Ma <maz@columbia.edu>
2011-03-31 13:54:09 +0000

    Add SOM cluster display
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7693 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/KMeansResult.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/KMeansResult.java
@@ -4,30 +4,40 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
 import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.biocollections.views.DSDataSetView;
 import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+import org.geworkbench.bison.model.clusters.CSSOMClusterDataSet;
+import org.geworkbench.bison.model.clusters.SOMCluster;
 
 /**
  * Result of K-Means Clustering analysis.
  * @author zm2165
  * @version $Id$
 */
-public class KMeansResult extends CSAncillaryDataSet<DSMicroarray> {
+public class KMeansResult extends CSSOMClusterDataSet {
 
 	private static final long serialVersionUID = -5747912398049332993L;
 	private String resultText;
 	private int clusterBy=0;
 	private ArrayList<List<String[]>> resultList = null;
 	private DSMicroarraySet<DSMicroarray> maSet;
+	private SOMCluster[][] graphResults;
 	
-	public KMeansResult(final DSMicroarraySet<DSMicroarray> maSet, String string, int clusterBy, String resultText,
-			ArrayList<List<String[]>> resultList) {
-		super(maSet, string);
-		this.clusterBy=clusterBy;
-		this.resultText=resultText;
+	
+	public KMeansResult(final DSMicroarraySet<DSMicroarray> maSet, String name, 
+			DSDataSetView<?> dataSetView, SOMCluster[][] graphResults, 
+				int clusterBy, ArrayList<List<String[]>> resultList) {
+		
+		super(graphResults, name, dataSetView);
+		this.graphResults=graphResults;
+		this.clusterBy=clusterBy;		
 		this.resultList=resultList;
-		this.maSet=maSet;
+		this.maSet=maSet;		
+	}
+	
+	public void CSSOMClusterDataSet(){
+		
 	}
 
 	public DSMicroarraySet<DSMicroarray> getMaSet(){
@@ -43,6 +53,9 @@ public class KMeansResult extends CSAncillaryDataSet<DSMicroarray> {
 	
 	public ArrayList<List<String[]>> getResultList(){
 		return resultList;
+	}
+	public SOMCluster[][] getGraphResults(){
+		return graphResults;
 	}
 	
 	public File getDataSetFile() {
