1716aced14a1e8bcf5f8e13af9f9bbc2bfe9d560
Zhou Ji <zji@c2b2.columbia.edu>
2011-04-20 14:24:06 +0000

    bug 2482 note 9266 step 1
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7757 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/bison/datastructure/biocollections/AdjacencyMatrix.java
@@ -0,0 +1,288 @@
+package org.geworkbench.bison.datastructure.biocollections;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+
+/**
+ * AdjacencyMatrix.
+ * 
+ * @author not attributable
+ * @version $Id$
+ */
+
+public class AdjacencyMatrix implements Serializable {
+
+	private static final long serialVersionUID = 2986018836859246187L;
+
+	private static Log log = LogFactory.getLog(AdjacencyMatrix.class);
+
+	public static class EdgeInfo implements Serializable {
+		private static final long serialVersionUID = 8884626375173162244L;
+		public float value;
+		public String type;
+
+		EdgeInfo(float value, String type) {
+			this.value = value;
+			this.type = type;
+		}
+	}
+
+	public enum NodeType {
+		MARKER, GENE_SYMBOL, PROBESET_ID, STRING, NUMERIC, OTHER
+	};
+
+	// integer Id is not not used for now.
+	public static class Node {
+		public NodeType type;
+		public DSGeneMarker marker;
+		public String stringId;
+		public int intId;
+
+		public Node(DSGeneMarker marker) {
+			this.type = NodeType.MARKER;
+			this.marker = marker;
+			stringId = null;
+			intId = -1;
+		}
+
+		public Node(NodeType type, String id) {
+			this.type = type;
+			stringId = id;
+			intId = -1;
+			marker = null;
+		}
+
+		Node(NodeType type, int id) {
+			this.type = type;
+			intId = id;
+			stringId = null;
+			marker = null;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Node))
+				return false;
+
+			Node node = (Node) obj;
+			if (node.type == NodeType.MARKER && this.type == NodeType.MARKER
+					&& node.marker.equals(this.marker)) {
+				return true;
+			} else if (node.type == this.type
+					&& node.stringId.equals(this.stringId))
+				return true;
+			else
+				return false;
+		}
+	}
+
+	private HashMap<Node, HashMap<Node, EdgeInfo>> geneRows = new HashMap<Node, HashMap<Node, EdgeInfo>>();
+
+	private final DSMicroarraySet<DSMicroarray> maSet;
+
+	private final String name;
+
+	private final Map<String, String> interactionTypeSifMap; // TODO check ?
+
+	private int edgeNumber = 0;
+	private int nodeNumber = 0;
+
+	public AdjacencyMatrix(String name,
+			final DSMicroarraySet<DSMicroarray> microarraySet) {
+		this.name = name;
+		maSet = microarraySet;
+		interactionTypeSifMap = null;
+		log.debug("AdjacencyMatrix created with label " + name
+				+ " and microarray set " + maSet.getDataSetName());
+	}
+
+	public AdjacencyMatrix(String name,
+			final DSMicroarraySet<DSMicroarray> microarraySet,
+			Map<String, String> interactionTypeSifMap) {
+		this.name = name;
+		maSet = microarraySet;
+		this.interactionTypeSifMap = interactionTypeSifMap;
+		log.debug("AdjacencyMatrix created with label " + name
+				+ " and microarray set " + maSet.getDataSetName()
+				+ ", with interaction type map");
+	}
+
+	/**
+	 * Returns a map with all the edges to geneId. This is only used by MRA
+	 * analysis.
+	 * 
+	 */
+	// this replace the original public HashMap<Integer, Float> get(int geneId)
+	public Set<DSGeneMarker> get(DSGeneMarker marker) {
+		Set<DSGeneMarker> set = new HashSet<DSGeneMarker>();
+		HashMap<Node, EdgeInfo> row = geneRows.get(new Node(marker));
+		if (row == null) {
+			return null;
+		}
+		for (Node id : row.keySet()) {
+			if (id.type == NodeType.MARKER)
+				set.add(id.marker);
+		}
+		return set;
+	}
+
+	/**
+	 * Add a node only. This is useful only when there is no edged from this
+	 * node.
+	 * 
+	 * @param geneId
+	 */
+	// this replaces public void addGeneRow(int geneId) {
+	public void addGeneRow(Node node) {
+		HashMap<Node, EdgeInfo> row = geneRows.get(node);
+		if (row == null) {
+			geneRows.put(node, new HashMap<Node, EdgeInfo>());
+			nodeNumber++;
+		}
+	}
+
+	public String getLabel() {
+		return name;
+	}
+
+	/**
+	 * Adds and edge between geneId1 and geneId2, indexes into the microarray
+	 * dataset
+	 * 
+	 * @param geneId1
+	 *            int
+	 * @param geneId2
+	 *            int
+	 * @param edge
+	 *            float
+	 */
+	/*
+	 * This replaces two methods: public void add(int geneId1, int geneId2,
+	 * float edge, String interaction) {
+	 * 
+	 * public void add(String geneId1, String geneId2, boolean
+	 * isGene1InMicroarray, boolean isGene2InMicroarray, float edge, String
+	 * interaction) {
+	 * 
+	 * the getMappedId functionality is supported by the node itself
+	 */
+	public void add(Node node1, Node node2, float edge, String interaction) {
+
+		HashMap<Node, EdgeInfo> row = geneRows.get(node1);
+		if (row == null) {
+			row = new HashMap<Node, EdgeInfo>();
+			geneRows.put(node1, row);
+			nodeNumber++;
+		}
+		row.put(node2, new EdgeInfo(edge, interaction));
+
+		// doing it both ways; [gene2 -> (gene1, edge)]
+		row = geneRows.get(node2);
+		if (row == null) {
+			row = new HashMap<Node, EdgeInfo>();
+			geneRows.put(node2, row);
+			nodeNumber++;
+		}
+		row.put(node1, new EdgeInfo(edge, interaction));
+
+		edgeNumber++;
+	}
+
+	// this method is not needed any more
+	// public int getMappedId(int geneId)
+	// }
+
+	public int getConnectionNo() {
+		return edgeNumber;
+	}
+
+	public int getNodeNumber() {
+		return nodeNumber;
+	}
+
+	public DSMicroarraySet<DSMicroarray> getMicroarraySet() {
+		return maSet;
+	}
+
+	public Map<String, String> getInteractionTypeSifMap() {
+		return interactionTypeSifMap;
+	}
+
+	public static class Edge {
+		public Node node1;
+		public Node node2;
+		public EdgeInfo info;
+
+		Edge(Node node1, Node node2, EdgeInfo info) {
+			this.node1 = node1;
+			this.node2 = node2;
+			this.info = info;
+		}
+	}
+
+	/**
+	 * Edge for those not from the input microarray dataset.
+	 * 
+	 */
+	public static class EdgeWithStringNode {
+		public String node1;
+		public String node2;
+		public EdgeInfo info;
+
+		EdgeWithStringNode(String node1, String node2, EdgeInfo info) {
+			this.node1 = node1;
+			this.node2 = node2;
+			this.info = info;
+		}
+	}
+
+	/**
+	 * 
+	 * @return all edges
+	 */
+	public List<Edge> getEdges() {
+		List<Edge> list = new ArrayList<Edge>();
+		for (Node node1 : geneRows.keySet()) {
+			Map<Node, AdjacencyMatrix.EdgeInfo> destGenes = geneRows.get(node1);
+			for (Node node2 : destGenes.keySet()) {
+				list.add(new Edge(node1, node2, destGenes.get(node2)));
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @return edges from a given node
+	 */
+	// replace public List<Edge> getEdges(int node1) {
+	// replace as well public List<EdgeWithStringNode>
+	// getEdgesNotInMicroarray(String node1) {
+	public List<Edge> getEdges(Node node1) {
+		List<Edge> list = new ArrayList<Edge>();
+		Map<Node, AdjacencyMatrix.EdgeInfo> destGenes = geneRows.get(node1);
+		for (Node node2 : destGenes.keySet()) {
+			list.add(new Edge(node1, node2, destGenes.get(node2)));
+		}
+
+		return list;
+	}
+
+	// replace public List<Integer> getNodes() and public List<String>
+	// getNodesNotInMicroarray()
+	public List<Node> getNodes() {
+		return new ArrayList<Node>(geneRows.keySet());
+	}
+
+}
---
+++ src/org/geworkbench/bison/datastructure/biocollections/AdjacencyMatrixDataSet.java
@@ -0,0 +1,188 @@
+package org.geworkbench.bison.datastructure.biocollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix.NodeType;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+import org.geworkbench.bison.util.RandomNumberGenerator;
+import org.geworkbench.parsers.InputFileFormatException;
+
+/**
+ * @author John Watkinson
+ * @version $Id$
+ */
+public class AdjacencyMatrixDataSet extends CSAncillaryDataSet<DSMicroarray> {
+
+	private static final long serialVersionUID = 2222442531807486171L;
+
+	static Log log = LogFactory.getLog(AdjacencyMatrixDataSet.class);
+
+    private AdjacencyMatrix matrix;
+
+    private final double threshold;
+    private String networkName;
+
+	@SuppressWarnings("unchecked")
+	public AdjacencyMatrixDataSet(final AdjacencyMatrix matrix, final double threshold, final String name, final String networkName, final DSMicroarraySet<? extends DSMicroarray> parent) {
+        super((DSDataSet<DSMicroarray>) parent, name);
+        setID(RandomNumberGenerator.getID());
+        this.matrix = matrix;
+        this.threshold = threshold;
+        this.networkName = networkName;
+    }
+
+    public void writeToFile(String fileName) {
+        File file = new File(fileName);
+
+        try {
+            file.createNewFile();
+            if (!file.canWrite()) {
+                JOptionPane.showMessageDialog(null, "Cannot write to specified file.");
+                return;
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            // if entry key is less than 0, for CNKB component, it means the gene is in currently selected microarray.
+            for (AdjacencyMatrix.Node node1 : matrix.getNodes()) {
+                String geneName = "unknown";
+                if (node1.type == NodeType.MARKER)
+                	geneName = node1.marker.getLabel();
+                writer.write(geneName + "\t");
+
+                for (AdjacencyMatrix.Edge edge : matrix.getEdges(node1)) {
+                    String geneName2 = "unknown";
+                    if (edge.node2.type == NodeType.MARKER)
+                    	geneName2 = edge.node2.marker.getLabel();
+                    writer.write(geneName2 + "\t" + edge.info.value + "\t");
+                }
+                writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            log.error(e);
+        }
+    }
+
+    public void readFromFile(String fileName, DSMicroarraySet<DSMicroarray> maSet) {
+    	try {
+			matrix = parseAdjacencyMatrix(fileName, maSet);
+		} catch (InputFileFormatException e) {
+			log.error(e);
+		}
+    }
+
+	public static AdjacencyMatrix parseAdjacencyMatrix(String fileName,
+			DSMicroarraySet<DSMicroarray> maSet)
+			throws InputFileFormatException {
+		int connectionsInstantiated = 0;
+		int connectionsIgnored = 0;
+
+        BufferedReader br = null;
+        AdjacencyMatrix matrix = new AdjacencyMatrix(fileName, maSet);
+
+        try {
+
+            //            readMappings(new File(name));
+            br = new BufferedReader(new FileReader(fileName));
+            try {
+                //                String line = br.readLine();
+                String line;
+                int ctr = 0;
+                //                while (br.ready()) {
+                while ((line = br.readLine()) != null) {
+                    if (ctr++ % 100 == 0) {
+                        log.debug("Reading line " + ctr);
+                    }
+                    //skip comments
+                    if (line.startsWith(">")) continue;
+                    if (line.length() > 0 && line.charAt(0) != '-') {
+                        StringTokenizer tr = new StringTokenizer(line, "\t:");
+
+                        //String geneAccess = new String(tr.nextToken());
+                        String strGeneId1 = new String(tr.nextToken());
+                        DSGeneMarker m = maSet.getMarkers().get(strGeneId1);
+                        if (m == null) { //we don't have this gene in our MicroarraySet
+                        	//we skip it
+                        	continue;
+                        }
+						while (tr.hasMoreTokens()) {
+							String strGeneId2 = new String(tr.nextToken());
+							DSGeneMarker m2 = maSet.getMarkers()
+									.get(strGeneId2);
+							if (m2 == null) { // we don't have this gene in our
+												// MicroarraySet
+								// we skip it
+								continue;
+							}
+							String strMi = new String(tr.nextToken());
+							float mi = Float.parseFloat(strMi);
+							if (m != m2) {
+								connectionsInstantiated++;
+								matrix.add(new AdjacencyMatrix.Node(m),
+										new AdjacencyMatrix.Node(m2), mi, null);
+								// this.addInteractionType2(geneId1, geneId2,
+								// mi);
+							} else {
+								connectionsIgnored++;
+							}
+						}
+
+                    }
+                    //                    line = br.readLine();
+                }
+                log.debug("Connections instantiated " + connectionsInstantiated);
+                log.debug("Connections ignored " + connectionsIgnored);
+                log.debug("Total processed " + (connectionsInstantiated + connectionsIgnored));
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            	throw new InputFileFormatException(ex.getMessage());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            	throw new InputFileFormatException(ex.getMessage());
+            }
+        } catch (FileNotFoundException ex3) {
+            ex3.printStackTrace();
+        	throw new InputFileFormatException(ex3.getMessage());
+        }
+
+        return matrix;
+	}
+
+    public AdjacencyMatrix getMatrix() {
+        return matrix;
+    }
+
+    public double getThreshold() {
+        return threshold;
+    }
+
+    public File getDataSetFile() {
+        // no-op
+        return null;
+    }
+
+    public void setDataSetFile(File file) {
+        // no-op
+    }
+
+    public String getNetworkName() {
+        return networkName;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
+    }
+}
