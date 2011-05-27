5ce21bc0d57e3a377d88a8a86fe630c824ea332d
Zhou Ji <zji@c2b2.columbia.edu>
2011-05-27 20:49:26 +0000

    see bug 2512 note 9593
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7946 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/GoAnalysisResult.java
+++ src/org/geworkbench/bison/datastructure/biocollections/GoAnalysisResult.java
@@ -200,7 +200,7 @@ public class GoAnalysisResult extends CSAncillaryDataSet<CSMicroarray> {
 	
 	private static int countUnexpectedEntrezId = 0;
 
-	public static void parseAnnotation(String annotationFileName) {
+	public static void parseAnnotation(String annotationFileName) throws IOException {
 		term2Gene.clear();
 		geneDetails.clear();
 		
@@ -214,6 +214,10 @@ public class GoAnalysisResult extends CSAncillaryDataSet<CSMicroarray> {
 					line = br.readLine();
 				line = line.substring(1, line.length()-2); // trimming the leading and trailing quotation mark
 				String[] fields = line.split("\",\"");
+				final int AFFY_COLUMN_COUNT = 41;
+				if(fields.length!=AFFY_COLUMN_COUNT) {
+					throw new IOException("Annotation file "+annotationFileName+" not recognized.");
+				}
 				String geneSymbolField = fields[ANNOTATION_INDEX_GENE_SYMBOL];
 				String biologicalProcess = fields[ANNOTATION_INDEX_BIOLOGICAL_PROCESS];
 				String cellularComponent = fields[ANNOTATION_INDEX_CELLULAR_COMPONENT];
@@ -240,9 +244,6 @@ public class GoAnalysisResult extends CSAncillaryDataSet<CSMicroarray> {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			log.error("Annotation map is not successfullly created due to FileNotException: "+e.getMessage());
-		} catch (IOException e) {
-			e.printStackTrace();
-			log.error("Annotation map is not successfullly created due to IOException: "+e.getMessage());
 		}
 		staticAnnotationFileName = annotationFileName;
 	}
