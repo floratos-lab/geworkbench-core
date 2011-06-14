26ab7775b5acc9d84115623411c1831c2aad7af4
Min You <my2248@c2b2.columbia.edu>
2011-06-14 19:01:28 +0000

    reverse back to previous version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@7988 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/biocollections/AdjacencyMatrix.java
+++ src/org/geworkbench/bison/datastructure/biocollections/AdjacencyMatrix.java
@@ -144,19 +144,11 @@ public class AdjacencyMatrix implements Serializable {
 		Set<DSGeneMarker> set = new HashSet<DSGeneMarker>();
 		HashMap<Node, EdgeInfo> row = geneRows.get(new Node(marker));
 		if (row == null) {
-			row = geneRows.get(new Node(NodeType.GENE_SYMBOL, marker.getGeneName()));			 
+			return null;
 		}
-		if ( row == null)
-		    return null;
 		for (Node id : row.keySet()) {
 			if (id.type == NodeType.MARKER)
 				set.add(id.marker);
-			else if (id.type == NodeType.GENE_SYMBOL)
-			{
-				DSGeneMarker m = maSet.getMarkers().get(id.stringId);			 
-				if ( m != null )
-				   set.add(maSet.getMarkers().get(id.stringId));
-			}
 		}
 		return set;
 	}
