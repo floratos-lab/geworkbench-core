56d554c468c2d64a95290e66568d869ea494143f
Zheng Ma <maz@columbia.edu>
2011-09-07 15:22:04 +0000

    modify local version for a uniform ideaResult with cagrid service version
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8256 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/bison/datastructure/bioobjects/IdeaResultDataSet.java
@@ -0,0 +1,62 @@
+package org.geworkbench.bison.datastructure.bioobjects;
+
+import java.io.File;
+import java.util.List;
+
+import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
+
+/**
+ * Result of IDEA analysis.
+ * @author zm2165
+ * @version $Id$
+*/
+public class IdeaResultDataSet extends CSAncillaryDataSet<DSMicroarray> {
+	private static final long serialVersionUID = 1728642489420856774L;
+
+	private List<IdeaGLoc> locList = null;
+	private List<IdeaGLoc> gocList = null;
+	private List<IdeaNode> nodeList=null;
+	private List<IdeaModule> moduleList=null;
+	private double pvalue=0.05;
+
+	public IdeaResultDataSet(final DSMicroarraySet<DSMicroarray> maSet, String string,
+			List<IdeaGLoc> locList, List<IdeaGLoc> gocList, List<IdeaNode> nodeList, List<IdeaModule> moduleList,double pvalue) {
+		super(maSet, string);
+		
+		this.locList = locList;
+		this.gocList = gocList;
+		this.nodeList = nodeList;
+		this.moduleList=moduleList;
+		this.pvalue=pvalue;
+	}
+
+	public File getDataSetFile() {
+		// no-op
+		return null;
+	}
+
+	public void setDataSetFile(File file) {
+		// no-op
+	}
+
+	public List<IdeaGLoc> getLocList() {
+		return locList;
+	}
+	public List<IdeaGLoc> getGocList() {
+		return gocList;
+	}
+
+	public List<IdeaNode> getNodeList() {
+		return nodeList;
+	}
+	
+	public List<IdeaModule> getModuleList() {
+		return moduleList;
+	}
+	
+	public double getPvalue(){
+		return pvalue;
+	}
+}
