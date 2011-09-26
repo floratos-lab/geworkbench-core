96dd700a6f23eeb17a5a6952cf10f088910f809b
Zhou Ji <zji@c2b2.columbia.edu>
2011-09-26 16:04:49 +0000

    a minor refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8328 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/builtin/projects/SaveTree.java
+++ src/org/geworkbench/builtin/projects/SaveTree.java
@@ -48,30 +48,36 @@ public class SaveTree implements Serializable {
 		}
 	}
 
+	static DSDataSet<?> getDSDataSet(ProjectTreeNode treeNode) {
+		if (treeNode instanceof DataSetNode) {
+			DataSetNode childNode = (DataSetNode) treeNode;
+			return childNode.dataFile;
+		} else if (treeNode instanceof ImageNode) {
+			ImageNode childNode = (ImageNode) treeNode;
+			DSDataSet<?> dataSet = childNode._aDataSet;
+			if (dataSet.getID() == null)
+				dataSet.setID(RandomNumberGenerator.getID());
+			return dataSet;
+		} else if (treeNode instanceof DataSetSubNode) {
+			DataSetSubNode childNode = (DataSetSubNode) treeNode;
+			DSDataSet<?> dataSet = childNode._aDataSet;
+			if (dataSet.getID() == null)
+				dataSet.setID(RandomNumberGenerator.getID());
+			return dataSet;
+		} else if (treeNode instanceof PendingTreeNode) {
+			PendingTreeNode childNode = (PendingTreeNode) treeNode;
+			return childNode.getDSDataSet();
+		}
+		return null; // unexpected;
+	}
+	
 	// Recursively build tree
 	private void addChildren(ProjectTreeNode node, DataSetSaveNode saveNode) {
 		Skin skin = (Skin) GeawConfigObject.getGuiWindow();
 		int n = node.getChildCount();
 		for (int i = 0; i < n; i++) {
 			ProjectTreeNode treeNode = (ProjectTreeNode) node.getChildAt(i);
-			DSDataSet<?> dataSet = null;
-			if (treeNode instanceof DataSetNode) {
-				DataSetNode childNode = (DataSetNode) treeNode;
-				dataSet = childNode.dataFile;
-			} else if (treeNode instanceof ImageNode) {
-				ImageNode childNode = (ImageNode) treeNode;
-				dataSet = childNode._aDataSet;
-				if (dataSet.getID() == null)
-					dataSet.setID(RandomNumberGenerator.getID());
-			} else if (treeNode instanceof DataSetSubNode) {
-				DataSetSubNode childNode = (DataSetSubNode) treeNode;
-				dataSet = childNode._aDataSet;
-				if (dataSet.getID() == null)
-					dataSet.setID(RandomNumberGenerator.getID());
-			} else if (treeNode instanceof PendingTreeNode) {
-				PendingTreeNode childNode = (PendingTreeNode) treeNode;
-				dataSet = childNode.getDSDataSet();
-			}
+			DSDataSet<?> dataSet = getDSDataSet(treeNode);
 
 			DataSetSaveNode childSave = new DataSetSaveNode(dataSet);
 			childSave.setCommandSelected(skin.getCommandLastSelected(dataSet));
