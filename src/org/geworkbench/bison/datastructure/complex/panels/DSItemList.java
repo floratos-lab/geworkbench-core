52ba0b20d4ef54605336a30715cb3f32ee2e53b6
Zhou Ji <zji@c2b2.columbia.edu>
2011-09-29 22:01:03 +0000

    bug 2044 note 10360
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8353 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/complex/panels/DSItemList.java
+++ src/org/geworkbench/bison/datastructure/complex/panels/DSItemList.java
@@ -17,20 +17,4 @@ public interface DSItemList <T extends DSNamed> extends List<T>, DSIdentifiable 
      */
     T get(String label);
 
-    /**
-     * Gets an item by item template.
-     *
-     * @param item the item to fetch. Only the {@link org.geworkbench.bison.datastructure.properties.DSNamed#getLabel()}
-     *             method will be used.
-     * @return the requested object, or <code>null</code> if it was not found.
-     */
-    T get(T item);
-
-    /**
-     * Renames the given named item. Map-based item lists can update their mapping.
-     *
-     * @param item  item to be renamed (but has not yet been renamed). Must be a member of the list.
-     * @param label new name for item.
-     */
-    void rename(T item, String label);
 }
