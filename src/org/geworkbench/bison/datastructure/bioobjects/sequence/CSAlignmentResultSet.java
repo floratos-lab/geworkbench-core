f2925fd68f13d2e670a3cd53f99ce99fdb560347
Min You <my2248@c2b2.columbia.edu>
2011-10-24 21:55:30 +0000

    fix for bug #2780
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@8454 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/sequence/CSAlignmentResultSet.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/sequence/CSAlignmentResultSet.java
@@ -1,6 +1,8 @@
 package org.geworkbench.bison.datastructure.bioobjects.sequence;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import org.geworkbench.bison.datastructure.biocollections.CSAncillaryDataSet;
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
@@ -8,125 +10,132 @@ import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
 import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 
-public class CSAlignmentResultSet extends CSAncillaryDataSet<DSBioObject> implements DSAlignmentResultSet {
+public class CSAlignmentResultSet extends CSAncillaryDataSet<DSBioObject>
+		implements DSAlignmentResultSet {
 
 	private static final long serialVersionUID = 3284079552620104447L;
 
-	public CSAlignmentResultSet(String fileName, String inputFile, DSDataSet<DSBioObject> dataSet) {
-        super(dataSet, "BLAST Result");
-        resultFile = new File(fileName);
-        fastaFile = new File(inputFile);
+	private File fastaFile = null;
+	private File resultFile = null;
 
-    }
+	private ArrayList<Vector<BlastObj>> blastDataSet = null;
+	private String summary = null;
+	private int totalHitCount;
 
-    /**
-     * @param fileName
-     * @param inputFile
-     * @param dataSet
-     */
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-	public CSAlignmentResultSet(String fileName, String inputFile, DSSequenceSet<? extends DSSequence> blastedParentdataSet, DSSequenceSet<? extends DSSequence> parentDataSet) {
-        super((DSDataSet)parentDataSet, "BLAST Result");
-        resultFile = new File(fileName);
-        fastaFile = new File(inputFile);
-        blastedParentDataSet = blastedParentdataSet;
+	/**
+	 * @param fileName
+	 * @param inputFile
+	 * @param dataSet
+	 */
+	@SuppressWarnings( { "unchecked", "rawtypes" })
+	public CSAlignmentResultSet(String resultFile,
+			ArrayList<Vector<BlastObj>> blastDataSet, String summary,
+			int totalHitCount, String fastaFile,
+			DSSequenceSet<? extends DSSequence> blastedParentdataSet,
+			DSSequenceSet<? extends DSSequence> parentDataSet) {
+		super((DSDataSet) parentDataSet, "BLAST Result");
+		this.resultFile = new File(resultFile);
+		this.blastDataSet = blastDataSet;
+		this.summary = summary;
+		this.totalHitCount = totalHitCount;
+		this.fastaFile = new File(fastaFile);
+		this.blastedParentDataSet = blastedParentdataSet;
 
-    }
+	}
 
+	public DSSequenceSet<? extends DSSequence> getBlastedParentDataSet() {
+		return blastedParentDataSet;
+	}
 
-    private File fastaFile = null;
-    private File resultFile = null;
+	public void setBlastedParentDataSet(
+			DSSequenceSet<? extends DSSequence> blastedParentDataSet) {
+		this.blastedParentDataSet = blastedParentDataSet;
+	}
 
-    public DSSequenceSet<? extends DSSequence> getBlastedParentDataSet() {
-        return blastedParentDataSet;
-    }
+	private DSSequenceSet<? extends DSSequence> blastedParentDataSet;
 
-    public void setBlastedParentDataSet(DSSequenceSet<? extends DSSequence> blastedParentDataSet) {
-        this.blastedParentDataSet = blastedParentDataSet;
-    }
+	/**
+	 * isDirty
+	 * 
+	 * @return boolean
+	 * @todo Implement this geaw.bean.microarray.MAMemoryStatus method
+	 */
+	public boolean isDirty() {
+		return false;
+	}
 
-    private DSSequenceSet<? extends DSSequence> blastedParentDataSet;
+	/**
+	 * setDirty
+	 * 
+	 * @param boolean0
+	 *            boolean
+	 * @todo Implement this geaw.bean.microarray.MAMemoryStatus method
+	 */
+	public void setDirty(boolean boolean0) {
+	}
 
-    public void setResultFile(String outputFilename) {
-        resultFile = new File(outputFilename);
-    }
+	/**
+	 * getDataSetName
+	 * 
+	 * @return String
+	 * @todo Implement this medusa.components.projects.IDataSet method
+	 */
+	public String getDataSetName() {
+		return resultFile.getName();
+	}
 
-    /**
-     * isDirty
-     *
-     * @return boolean
-     * @todo Implement this geaw.bean.microarray.MAMemoryStatus method
-     */
-    public boolean isDirty() {
-        return false;
-    }
+	public ArrayList<Vector<BlastObj>> getBlastDataSet() {
+		return blastDataSet;
+	}
 
-    /**
-     * setDirty
-     *
-     * @param boolean0 boolean
-     * @todo Implement this geaw.bean.microarray.MAMemoryStatus method
-     */
-    public void setDirty(boolean boolean0) {
-    }
+	public File getDataSetFile() {
 
-    /**
-     * getDataSetName
-     *
-     * @return String
-     * @todo Implement this medusa.components.projects.IDataSet method
-     */
-    public String getDataSetName() {
-        // System.out.println("in get Datasetname " + resultFile.getAbsolutePath());
-        return resultFile.getName();
-    }
+		return fastaFile;
+	}
 
-    public String getResultFilePath() {
-        if (resultFile.canRead()) {
-            return resultFile.getAbsolutePath();
-        }
-        return null;
-    }
+	public void setDataSetFile(File _file) {
+		fastaFile = _file;
+	}
 
-    public File getDataSetFile() {
-
-        return fastaFile;
-    }
-
-    public void setDataSetFile(File _file) {
-        fastaFile = _file;
-    }
-
-    /**
-     * @param ads IAncillaryDataSet
-     * @return boolean
-     */
-    @SuppressWarnings("unchecked")
+	/**
+	 * @param ads
+	 *            IAncillaryDataSet
+	 * @return boolean
+	 */
+	@SuppressWarnings("unchecked")
 	public boolean equals(Object ads) {
-        if (ads instanceof DSAncillaryDataSet) {
-            return getDataSetName() ==
-                    ((DSAncillaryDataSet<DSBioObject>) ads).getDataSetName();
-        } else {
-            return false;
-        }
-    }
+		if (ads instanceof DSAncillaryDataSet) {
+			return getDataSetName() == ((DSAncillaryDataSet<DSBioObject>) ads)
+					.getDataSetName();
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * getFile
-     *
-     * @return File
-     * @todo Implement this medusa.components.projects.IDataSet method
-     */
-    public File getFile() {
-        return resultFile;
-    }
+	/**
+	 * getFile
+	 * 
+	 * @return File
+	 */
+	public File getFile() {
+		return resultFile;
+	}
 
-    /**
-     * writeToFile
-     *
-     * @param fileName String
-     */
-    public void writeToFile(String fileName) {
-    }
+	public String getSummary() {
+		return summary;
+	}
+
+	public int getHitCount() {
+		return totalHitCount;
+	}
+
+	/**
+	 * writeToFile
+	 * 
+	 * @param fileName
+	 *            String
+	 */
+	public void writeToFile(String fileName) {
+	}
 
 }
--- src/org/geworkbench/bison/datastructure/bioobjects/sequence/DSAlignmentResultSet.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/sequence/DSAlignmentResultSet.java
@@ -1,16 +1,25 @@
 package org.geworkbench.bison.datastructure.bioobjects.sequence;
 
 import org.geworkbench.bison.datastructure.biocollections.DSAncillaryDataSet;
+import org.geworkbench.bison.datastructure.biocollections.sequences.DSSequenceSet;
 import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.RandomAccess;
+import java.util.Vector;
 
 /**
  * @author John Watkinson
  * @version $Id$
  */
-public interface DSAlignmentResultSet extends RandomAccess, Cloneable, DSAncillaryDataSet<DSBioObject>, Serializable {
+public interface DSAlignmentResultSet extends RandomAccess, Cloneable,
+		DSAncillaryDataSet<DSBioObject>, Serializable {
+	
+	public DSSequenceSet<? extends DSSequence> getBlastedParentDataSet();
+	public ArrayList<Vector<BlastObj>> getBlastDataSet(); 
+	public String getSummary(); 
+	public int getHitCount();
 
-    String getResultFilePath();
+	 
 }
