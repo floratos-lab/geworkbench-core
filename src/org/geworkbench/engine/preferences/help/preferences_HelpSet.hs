2cf358c01fc8b1374b4bda0913b81d6dab95e9dd
Ken Smith <kcs3@columbia.edu>
2012-03-13 23:53:57 +0000

    Add help for Preferences in 2.3.0.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9164 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/engine/preferences/help/preferences_HelpSet.hs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<?TestTarget The help set file for the Preferences menu.?>
+
+<helpset version="1.0">
+
+	<!-- title -->
+
+	<title>Preferences</title>
+
+	<!-- maps -->
+
+	<maps>
+
+		<homeID>overview</homeID>
+
+		<mapref location="preferences_Map.jhm"/>
+
+	</maps>
+
+	<!-- views -->
+
+	<view>
+
+		<name>TOC</name>
+
+		<label>Preferences</label>
+
+		<type>javax.help.TOCView</type>
+
+		<data>preferences_toc.xml</data>
+
+	</view>
+
+</helpset>
+
