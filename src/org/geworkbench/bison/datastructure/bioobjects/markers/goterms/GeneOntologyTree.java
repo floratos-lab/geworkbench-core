364bfea6cfa99878a55a89af33799ba3a7a2ebe6
Ken Smith <kcs3@columbia.edu>
2012-05-31 15:32:56 +0000

    Updated error message for case where cannot download remote OBO file.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9510 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/bison/datastructure/bioobjects/markers/goterms/GeneOntologyTree.java
+++ src/org/geworkbench/bison/datastructure/bioobjects/markers/goterms/GeneOntologyTree.java
@@ -473,7 +473,7 @@ public class GeneOntologyTree {
 				public void run() {
 					JOptionPane.showMessageDialog(null,
 						    "Remote obo file was not loaded succesfully due to connection problem:\n>> "+e.getMessage()
-						    +"\nLocal copy loaded instead.",
+						    +"\nAttempting to open local copy.",
 						    "Obo file warning",
 						    JOptionPane.WARNING_MESSAGE);
 				}
