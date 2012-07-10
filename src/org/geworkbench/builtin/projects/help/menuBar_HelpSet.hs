780e7862b493d1caa810b13c659f69492e5aff56
Ken Smith <kcs3@columbia.edu>
2012-07-10 18:49:37 +0000

    Add Menu Bar help section.  Update "File Format" help.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9649 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbench/builtin/projects/help/menuBar_HelpSet.hs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<?TestTarget The help set file for the top-level Menu Bar.?>
+
+<helpset version="1.0">
+
+	<!-- title -->
+
+	<title>Menu Bar</title>
+
+	<!-- maps -->
+
+	<maps>
+
+		<homeID>overview</homeID>
+
+		<mapref location="menuBar_Map.jhm"/>
+
+	</maps>
+
+	<!-- views -->
+
+	<view>
+
+		<name>TOC</name>
+
+		<label>Menu Bar</label>
+
+		<type>javax.help.TOCView</type>
+
+		<data>menuBar_toc.xml</data>
+
+	</view>
+
+</helpset>
+
