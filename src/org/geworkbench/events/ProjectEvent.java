78082c77752ff655fe724dbd79fbb4af5a2093b7
Zhou Ji <zji@c2b2.columbia.edu>
2012-09-14 20:30:29 +0000

    minor cleaning-ups: mainly  to remove deprecated methods
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench@9926 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbench/events/ProjectEvent.java
+++ src/org/geworkbench/events/ProjectEvent.java
@@ -5,10 +5,6 @@ import org.apache.commons.logging.LogFactory;
 import org.geworkbench.bison.datastructure.biocollections.DSDataSet;
 import org.geworkbench.bison.datastructure.bioobjects.DSBioObject;
 import org.geworkbench.builtin.projects.ProjectTreeNode;
-import org.geworkbench.builtin.projects.DataSetSubNode;
-import org.geworkbench.builtin.projects.DataSetNode;
-
-import javax.swing.tree.TreeNode;
 
 /**
  * <p>Copyright: Copyright (c) 2003</p>
@@ -21,11 +17,6 @@ public class ProjectEvent {
 
 	static Log log = LogFactory.getLog(ProjectEvent.class);
 	
-	@Deprecated
-    public static final String CLEARED = "Project Cleared";
-	@Deprecated
-    public static final String SELECTED = "Node Selected";
-
     private final Message value;
     private final DSDataSet<? extends DSBioObject> dataSet;
     private final ProjectTreeNode node;
@@ -38,44 +29,14 @@ public class ProjectEvent {
         this.node = node;
     }
 
-    /**
-     * It is recommended to used the version using enum instead of String
-     */
-    @Deprecated
-    public String getMessage() {
-    	switch(value) {
-    	case CLEAR: return CLEARED;
-    	case SELECT: return SELECTED;
-    	default: return "No Message";
-    	}
-    }
-    
     public Message getValue() {
     	return value;
     }
 
-    @SuppressWarnings("rawtypes")
-	public DSDataSet getDataSet() {
+	public DSDataSet<? extends DSBioObject> getDataSet() {
         return dataSet;
     }
 
-    /**
-     * This should be ProjectTreeNode's responsibility, not this class's. 
-     */
-    @Deprecated
-    @SuppressWarnings("rawtypes")
-	public DSDataSet getParent() {
-		if (node instanceof DataSetSubNode) {
-			DataSetSubNode subNode = (DataSetSubNode) node;
-			TreeNode parent = subNode.getParent();
-			if (parent instanceof DataSetNode) {
-				return ((DataSetNode) parent).getDataset();
-			}
-		}
-
-        return null;
-    }
-    
     public ProjectTreeNode getTreeNode() { return node; }
 
 }
